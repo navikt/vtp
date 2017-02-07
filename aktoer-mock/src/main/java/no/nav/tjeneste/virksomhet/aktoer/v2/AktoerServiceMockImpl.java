93f5a74664d95131f6db9cc34c70d4f46a450094
Bjørn Thore Storesund <bjorn.thore.storesund@nav.no>
2017-02-07 15:13:58 +0100

    PK-37695 PKBIE-45 Etablert aktørmock

---
+++ aktoer-mock/src/main/java/no/nav/tjeneste/virksomhet/aktoer/v2/AktoerServiceMockImpl.java
@@ -0,0 +1,100 @@
+package no.nav.tjeneste.virksomhet.aktoer.v2;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.tjeneste.virksomhet.aktoer.v2.binding.AktoerV2;
+import no.nav.tjeneste.virksomhet.aktoer.v2.binding.HentAktoerIdForIdentPersonIkkeFunnet;
+import no.nav.tjeneste.virksomhet.aktoer.v2.binding.HentIdentForAktoerIdPersonIkkeFunnet;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.AktoerIder;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentAktoerIdForIdentListeRequest;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentAktoerIdForIdentListeResponse;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentAktoerIdForIdentRequest;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentAktoerIdForIdentResponse;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentIdentForAktoerIdListeRequest;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentIdentForAktoerIdListeResponse;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentIdentForAktoerIdRequest;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.HentIdentForAktoerIdResponse;
+import no.nav.tjeneste.virksomhet.aktoer.v2.meldinger.IdentDetaljer;
+
+@WebService(name = "Aktoer_v2", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2")
+public class AktoerServiceMockImpl implements AktoerV2 {
+
+    private static final Logger log = LoggerFactory.getLogger(AktoerServiceMockImpl.class);
+
+    @Override
+    @WebMethod(action = "http://nav.no/tjeneste/virksomhet/aktoer/v2/Aktoer_v2/hentIdentForAktoerIdRequest")
+    @WebResult(name = "hentIdentForAktoerIdResponse", targetNamespace = "")
+    @RequestWrapper(localName = "hentIdentForAktoerId", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentIdentForAktoerId")
+    @ResponseWrapper(localName = "hentIdentForAktoerIdResponse", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentIdentForAktoerIdResponse")
+    public HentIdentForAktoerIdResponse hentIdentForAktoerId(
+            @WebParam(name = "hentIdentForAktoerIdRequest", targetNamespace = "")
+            HentIdentForAktoerIdRequest hentIdentForAktoerIdRequest)
+            throws HentIdentForAktoerIdPersonIkkeFunnet {
+        log.info("hentIdentForAktoerId: " + hentIdentForAktoerIdRequest.getAktoerId());
+        HentIdentForAktoerIdResponse response = new HentIdentForAktoerIdResponse();
+        response.setIdent("123");
+        return response;
+    }
+
+    @Override
+    @WebMethod(action = "http://nav.no/tjeneste/virksomhet/aktoer/v2/Aktoer_v2/hentAktoerIdForIdentRequest")
+    @WebResult(name = "hentAktoerIdForIdentResponse", targetNamespace = "")
+    @RequestWrapper(localName = "hentAktoerIdForIdent", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentAktoerIdForIdent")
+    @ResponseWrapper(localName = "hentAktoerIdForIdentResponse", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentAktoerIdForIdentResponse")
+    public HentAktoerIdForIdentResponse hentAktoerIdForIdent(
+        @WebParam(name = "hentAktoerIdForIdentRequest", targetNamespace = "")
+        HentAktoerIdForIdentRequest hentAktoerIdForIdentRequest)
+        throws HentAktoerIdForIdentPersonIkkeFunnet {
+        log.info("hentIdentForAktoerId: " + hentAktoerIdForIdentRequest.getIdent());
+        HentAktoerIdForIdentResponse response = new HentAktoerIdForIdentResponse();
+        response.setAktoerId("456");
+        return response;
+    }
+
+    @Override
+    @WebMethod(action = "http://nav.no/tjeneste/virksomhet/aktoer/v2/Aktoer_v2/hentAktoerIdForIdentListeRequest")
+    @WebResult(name = "hentAktoerIdForIdentListeResponse", targetNamespace = "")
+    @RequestWrapper(localName = "hentAktoerIdForIdentListe", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentAktoerIdForIdentListe")
+    @ResponseWrapper(localName = "hentAktoerIdForIdentListeResponse", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentAktoerIdForIdentListeResponse")
+    public HentAktoerIdForIdentListeResponse hentAktoerIdForIdentListe(
+        @WebParam(name = "hentAktoerIdForIdentListeRequest", targetNamespace = "")
+        HentAktoerIdForIdentListeRequest hentAktoerIdForIdentListeRequest) {
+        log.info("hentIdentForAktoerId");
+        HentAktoerIdForIdentListeResponse response = new HentAktoerIdForIdentListeResponse();
+        AktoerIder aktoerIder = new AktoerIder();
+        aktoerIder.setAktoerId("789");
+        IdentDetaljer identDetaljer = new IdentDetaljer();
+        aktoerIder.setGjeldendeIdent(identDetaljer);
+        response.getAktoerListe().add(aktoerIder);
+        return response;
+    }
+
+    @Override
+    @WebMethod(action = "http://nav.no/tjeneste/virksomhet/aktoer/v2/Aktoer_v2/hentIdentForAktoerIdListeRequest")
+    @WebResult(name = "hentIdentForAktoerIdListeResponse", targetNamespace = "")
+    @RequestWrapper(localName = "hentIdentForAktoerIdListe", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentIdentForAktoerIdListe")
+    @ResponseWrapper(localName = "hentIdentForAktoerIdListeResponse", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.HentIdentForAktoerIdListeResponse")
+    public HentIdentForAktoerIdListeResponse hentIdentForAktoerIdListe(
+        @WebParam(name = "hentIdentForAktoerIdListeRequest", targetNamespace = "")
+        HentIdentForAktoerIdListeRequest hentIdentForAktoerIdListeRequest) {
+        log.info("hentIdentForAktoerId");
+        HentIdentForAktoerIdListeResponse response = new HentIdentForAktoerIdListeResponse();
+        return response;
+    }
+
+    @Override
+    @WebMethod(action = "http://nav.no/tjeneste/virksomhet/aktoer/v2/Aktoer_v2/pingRequest")
+    @RequestWrapper(localName = "ping", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.Ping")
+    @ResponseWrapper(localName = "pingResponse", targetNamespace = "http://nav.no/tjeneste/virksomhet/aktoer/v2", className = "no.nav.tjeneste.virksomhet.aktoer.v2.PingResponse")
+    public void ping() {
+        log.info("Ping mottatt og besvart");
+    }
+}
---
+++ mock-server/src/main/java/no/nav/engangsstønad/mock/MockServer.java
@@ -0,0 +1,54 @@
+package no.nav.engangsstønad.mock;
+
+import java.lang.reflect.Method;
+
+import javax.xml.ws.Endpoint;
+
+import org.eclipse.jetty.http.spi.HttpSpiContextHandler;
+import org.eclipse.jetty.http.spi.JettyHttpContext;
+import org.eclipse.jetty.http.spi.JettyHttpServer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+
+import com.sun.net.httpserver.HttpContext;
+
+public class MockServer {
+
+    private static HttpContext context;
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(7779);
+        ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+        server.setHandler(contextHandlerCollection);
+        server.start();
+        context = buildHttpContext(server, "/ws");
+        publishService("no.nav.tjeneste.virksomhet.aktoer.v2.AktoerServiceMockImpl");
+        // access wsdl on http://localhost:7779/ws/Aktoer_v2?wsdl
+    }
+
+    public static void publishService(String classname) {
+        try {
+            Class<?> cl = Class.forName(classname);
+            Object ws = cl.newInstance();
+            Endpoint endpoint = Endpoint.create(ws);
+            endpoint.publish(context);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static HttpContext buildHttpContext(Server server, String contextString) {
+        JettyHttpServer jettyHttpServer = new JettyHttpServer(server, true);
+        JettyHttpContext ctx = (JettyHttpContext) jettyHttpServer.createContext(contextString);
+        try {
+            Method method = JettyHttpContext.class.getDeclaredMethod("getJettyContextHandler");
+            method.setAccessible(true);
+            HttpSpiContextHandler contextHandler = (HttpSpiContextHandler) method.invoke(ctx);
+            contextHandler.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ctx;
+    }}
