4177fe9e8f3a5699195b9a49bd7a877b315a66be
Espen Svendsen <espen.svendsen@nav.no>
2017-02-21 10:02:44 +0100

    PK-37692 PKBIE-114 Skiller ut PersonBygger som en gjenbrukbar klasse

---
+++ tps-mock/modell/src/main/java/no/nav/tjeneste/virksomhet/person/v2/modell/PersonBygger.java
@@ -0,0 +1,124 @@
+package no.nav.tjeneste.virksomhet.person.v2.modell;
+
+import no.nav.tjeneste.virksomhet.person.v2.informasjon.*;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Objects;
+
+public class PersonBygger {
+    private String fnr;
+    private String fornavn;
+    private String etternavn;
+    private final Kjønn kjønn;
+    private LocalDate fødselsdato;
+
+    PersonBygger(String fnr, String fornavn, String etternavn, Kjønn kjønn) {
+        Objects.requireNonNull(fnr, "Fødselsnummer er obligatorisk");
+        Objects.requireNonNull(kjønn, "Kjønn er obligatorisk");
+        Objects.requireNonNull(fornavn, "Fornavn er obligatorisk");
+        Objects.requireNonNull(etternavn, "Etternavn er obligatorisk");
+        this.fnr = fnr;
+        this.kjønn = kjønn;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+    }
+
+    public String getFnr() {
+        return fnr;
+    }
+
+    public PersonBygger medFødseldato(LocalDate fødselsdato) {
+        this.fødselsdato = fødselsdato;
+        return this;
+    }
+
+    public Person bygg() {
+        Person person = new Person();
+
+        // Ident
+        NorskIdent norskIdent = new NorskIdent();
+        norskIdent.setIdent(fnr);
+        Personidenter personidenter = new Personidenter();
+        personidenter.setValue("fnr");
+        norskIdent.setType(personidenter);
+        person.setIdent(norskIdent);
+
+        // Kjønn
+        Kjoenn kjonn = new Kjoenn();
+        Kjoennstyper kjonnstype = new Kjoennstyper();
+        kjonnstype.setValue(kjønn.verdi);
+        kjonn.setKjoenn(kjonnstype);
+        person.setKjoenn(kjonn);
+
+        // Fødselsdato
+        Foedselsdato fodselsdato = new Foedselsdato();
+        XMLGregorianCalendar xcal;
+        if (fødselsdato == null) {
+            xcal = tilXmlGregorian(fnr);
+        } else {
+            xcal = tilXmlGregorian(fødselsdato);
+        }
+        fodselsdato.setFoedselsdato(xcal);
+        person.setFoedselsdato(fodselsdato);
+
+        // Navn
+        Personnavn personnavn   = new Personnavn();
+        personnavn.setEtternavn(etternavn.toUpperCase());
+        personnavn.setFornavn(fornavn.toUpperCase());
+        personnavn.setSammensattNavn(etternavn.toUpperCase() + " " + fornavn.toUpperCase());
+        person.setPersonnavn(personnavn);
+
+        // I fremtiden: Legge til flere felter, som barn osv
+
+        return person;
+    }
+
+    private XMLGregorianCalendar tilXmlGregorian(LocalDate fødselsdato) {
+        XMLGregorianCalendar xcal;
+        try {
+            GregorianCalendar gcal = GregorianCalendar.from(fødselsdato.atStartOfDay(ZoneId.systemDefault()));
+            xcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
+        } catch (DatatypeConfigurationException e) {
+            throw new IllegalStateException("Kunne ikke konvertere dato", e);
+        }
+        return xcal;
+    }
+
+    private XMLGregorianCalendar tilXmlGregorian(String fødselsnummer) {
+        XMLGregorianCalendar xcal;
+        try {
+            // Simpel algoritme for å konvertere fnr. Må utbedres ved behov.
+            DateFormat format = new SimpleDateFormat("ddMMyy");
+            Date dato = format.parse(fødselsnummer.substring(0, 6));
+            GregorianCalendar gcal = new GregorianCalendar();
+            gcal.setTime(dato);
+            xcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
+        } catch (DatatypeConfigurationException | ParseException e) {
+            throw new IllegalStateException("Kunne ikke konvertere dato", e);
+        }
+        return xcal;
+    }
+
+
+
+
+    enum Kjønn {
+        MANN("M"),
+        KVINNE("K");
+
+        String verdi;
+
+        Kjønn(String verdi) {
+            this.verdi = verdi;
+        }
+    }
+}
--- tps-mock/modell/src/main/java/no/nav/tjeneste/virksomhet/person/v2/modell/TpsRepo.java
+++ tps-mock/modell/src/main/java/no/nav/tjeneste/virksomhet/person/v2/modell/TpsRepo.java
@@ -2,17 +2,12 @@ package no.nav.tjeneste.virksomhet.person.v2.modell;
 
 import no.nav.tjeneste.virksomhet.person.v2.informasjon.*;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 
-import static java.time.LocalDate.of;
-import static no.nav.tjeneste.virksomhet.person.v2.modell.TpsRepo.Kjønn.KVINNE;
-import static no.nav.tjeneste.virksomhet.person.v2.modell.TpsRepo.Kjønn.MANN;
+import static no.nav.tjeneste.virksomhet.person.v2.modell.PersonBygger.Kjønn.KVINNE;
+import static no.nav.tjeneste.virksomhet.person.v2.modell.PersonBygger.Kjønn.MANN;
 
 public class TpsRepo {
 
@@ -22,9 +17,18 @@ public class TpsRepo {
     private static Map<String, Long> AKTØR_ID_VED_FNR = new HashMap<>();
     private static Map<String, Person> PERSON_VED_FNR = new HashMap<>();
 
+    // Konstanter for standardbrukere (kan refereres eksternt)
+    public static final long STD_KVINNE_AKTØR_ID = 1000021552067L;
+    public static final String STD_KVINNE_FNR = "06016921295";
+    public static final String STD_KVINNE_FORNAVN = "MARIA L BELANDRES";
+    public static final String STD_KVINNE_ETTERNAVN = "HOLMSEN";
+    public static final int STD_KVINNE_FØDT_DAG = 06;
+    public static final int STD_KVINNE_FØDT_DAG_MND = 01;
+    public static final int STD_KVINNE_FØDT_DAG_ÅR = 1969;
+
     // Svarteliste
-    private static final String FNR_TRIGGER_SERVICE_UNAVAILABLE = "1111111111l";
-    private static final long AKTOER_ID_SERVICE_UNAVAILABLE = 2L;
+    public static final String FNR_TRIGGER_SERVICE_UNAVAILABLE = "1111111111l";
+    public static final long AKTOER_ID_SERVICE_UNAVAILABLE = 2L;
 
     public static synchronized TpsRepo init() {
         if(instance == null) {
@@ -35,9 +39,10 @@ public class TpsRepo {
 
     private TpsRepo() {
         List<TpsPerson> tpspersoner = new ArrayList<>();
-        tpspersoner.add(new TpsPersonBygger(1L, "13107221234", "ANNE-BERIT", "HJARTDAL", KVINNE).bygg());
-        tpspersoner.add(new TpsPersonBygger(3L, "01017912345", "LIV HENRIETTE", "LARSEN ULLMANN", KVINNE).bygg());
-        tpspersoner.add(new TpsPersonBygger(4L, "01018012345", "ANDERS", "ANDERSEN", MANN).bygg());
+        tpspersoner.add(new TpsPerson(STD_KVINNE_AKTØR_ID, new PersonBygger(STD_KVINNE_FNR, STD_KVINNE_FORNAVN, STD_KVINNE_ETTERNAVN, KVINNE)));
+        tpspersoner.add(new TpsPerson(1000021543419L, new PersonBygger("06016518156", "AL-HAMIDI", "KHADIM MUJULY H", MANN)));
+        tpspersoner.add(new TpsPerson(1000076788465L, new PersonBygger("41014100138", "BALLARIN", "AYORA MANUEL", MANN)
+                .medFødseldato(LocalDate.of(1941, Month.JANUARY, 1))));
 
         for (TpsPerson tpsPerson : tpspersoner) {
             FNR_VED_AKTØR_ID.put(tpsPerson.aktørId, tpsPerson.fnr);
@@ -77,90 +82,12 @@ public class TpsRepo {
     class TpsPerson {
         private final long aktørId;
         private final String fnr;
-
         private final Person person;
-        TpsPerson(long aktørId, String fnr, Person person) {
+
+        TpsPerson(long aktørId, PersonBygger personBygger) {
             this.aktørId = aktørId;
-            this.fnr = fnr;
-            this.person = person;
-        }
-    }
-
-    enum Kjønn {
-        MANN("M"),
-        KVINNE("K");
-
-        private String verdi;
-
-        Kjønn(String verdi) {
-            this.verdi = verdi;
-        }
-    }
-
-    class TpsPersonBygger {
-
-        private String fnr;
-        private String fornavn;
-        private String etternavn;
-        private long aktørId;
-        private final Kjønn kjønn;
-
-        TpsPersonBygger(long aktørId, String fnr, String fornavn, String etternavn, Kjønn kjønn) {
-            Objects.requireNonNull(fnr, "Fødselsnummer er obligatorisk");
-            Objects.requireNonNull(kjønn, "Kjønn er obligatorisk");
-            Objects.requireNonNull(fornavn, "Fornavn er obligatorisk");
-            Objects.requireNonNull(etternavn, "Etternavn er obligatorisk");
-            this.aktørId = aktørId;
-            this.fnr = fnr;
-            this.kjønn = kjønn;
-            this.fornavn = fornavn;
-            this.etternavn = etternavn;
-        }
-
-        TpsPerson bygg() {
-            Person person = new Person();
-
-            // Ident
-            NorskIdent norskIdent = new NorskIdent();
-            norskIdent.setIdent(fnr);
-            Personidenter personidenter = new Personidenter();
-            personidenter.setValue("fnr");
-            norskIdent.setType(personidenter);
-            person.setIdent(norskIdent);
-
-            // Kjønn
-            Kjoenn kjonn = new Kjoenn();
-            Kjoennstyper kjonnstype = new Kjoennstyper();
-            kjonnstype.setValue(kjønn.verdi);
-            kjonn.setKjoenn(kjonnstype);
-            person.setKjoenn(kjonn);
-
-            // Fødselsdato
-            Foedselsdato fodselsdato = new Foedselsdato();
-            XMLGregorianCalendar xcal;
-            try {
-                // Simpel algoritme for å konvertere fnr. Må utbedres ved behov.
-                DateFormat format = new SimpleDateFormat("ddmmyy");
-                Date dato = format.parse(fnr.substring(0, 6));
-                GregorianCalendar gcal = new GregorianCalendar();
-                gcal.setTime(dato);
-                xcal = DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal);
-            } catch (DatatypeConfigurationException | ParseException e) {
-                throw new IllegalStateException("Kunne ikke konvertere dato", e);
-            }
-            fodselsdato.setFoedselsdato(xcal);
-            person.setFoedselsdato(fodselsdato);
-
-            // Navn
-            Personnavn personnavn   = new Personnavn();
-            personnavn.setEtternavn(etternavn.toUpperCase());
-            personnavn.setFornavn(fornavn.toUpperCase());
-            personnavn.setSammensattNavn(etternavn.toUpperCase() + " " + fornavn.toUpperCase());
-            person.setPersonnavn(personnavn);
-
-            // I fremtiden: Legge til flere felter, som barn osv
-
-            return new TpsPerson(aktørId, fnr, person);
+            this.fnr = personBygger.getFnr();
+            this.person = personBygger.bygg();
         }
     }
 
