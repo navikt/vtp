3178ba26e35bd603cb7acc66b09390d3551e6a1e
Ramesh Shiraddi <ramesh.shiraddi@nav.no>
2017-03-23 16:47:47 +0100

    PK-38816 : Lagt mock response for hentJournalpostListe service

--- joark-mock/journal/src/main/java/no/nav/tjeneste/virksomhet/journal/v2/JournalServiceMockImpl.java
+++ joark-mock/journal/src/main/java/no/nav/tjeneste/virksomhet/journal/v2/JournalServiceMockImpl.java
@@ -7,6 +7,13 @@ import no.nav.tjeneste.virksomhet.journal.v2.binding.HentDokumentURLSikkerhetsbe
 import no.nav.tjeneste.virksomhet.journal.v2.binding.HentJournalpostListeSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.journal.v2.binding.JournalV2;
 import no.nav.tjeneste.virksomhet.journal.v2.feil.DokumentIkkeFunnet;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.Arkivfiltyper;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.DokumentInnhold;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.DokumentinfoRelasjon;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.Dokumenttyper;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.JournalfoertDokumentInfo;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.Journalpost;
+import no.nav.tjeneste.virksomhet.journal.v2.informasjon.Variantformater;
 import no.nav.tjeneste.virksomhet.journal.v2.meldinger.HentDokumentRequest;
 import no.nav.tjeneste.virksomhet.journal.v2.meldinger.HentDokumentResponse;
 import no.nav.tjeneste.virksomhet.journal.v2.meldinger.HentDokumentURLRequest;
@@ -20,17 +27,32 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 @WebService(name = "Journal_v2", targetNamespace = "http://nav.no/tjeneste/virksomhet/journal/v2")
 public class JournalServiceMockImpl implements JournalV2 {
     private static final Logger LOG = LoggerFactory.getLogger(JournalServiceMockImpl.class);
+    private static final String FILTYPE_XML = "XML";
+    private static final String VARIANTFORMAT_ORIGINAL = "ORIGINAL";
+    private static final String DOKUMENT_TYPE = "00001";
+    private static final LocalDateTime NOW = LocalDateTime.now();
+    private static final String VARIANTFORMAT_ARKIV = "ARKIV";
+    private static final String FILTYPE_PDF = "PDF";
+    private static final String JOURNAL_ID = "42";
+    private static final String DOKUMENT_ID = "66";
 
     @WebMethod(action = "http://nav.no/tjeneste/virksomhet/journal/v2/Journal_v2/hentJournalpostListeRequest")
     @WebResult(name = "Response", targetNamespace = "")
@@ -41,7 +63,15 @@ public class JournalServiceMockImpl implements JournalV2 {
             @WebParam(name = "Request", targetNamespace = "")
                     HentJournalpostListeRequest request)
             throws HentJournalpostListeSikkerhetsbegrensning {
-        throw new UnsupportedOperationException("Ikke implementert");
+        HentJournalpostListeResponse hentJournalpostListeResponse = new HentJournalpostListeResponse();
+        hentJournalpostListeResponse.getJournalpostListe().addAll(Arrays.asList(
+                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 1"),
+                createJournalpost(FILTYPE_XML, VARIANTFORMAT_ARKIV, "Dokument 2"),
+                createJournalpost(FILTYPE_XML, VARIANTFORMAT_ORIGINAL, "Dokument 3"),
+                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ORIGINAL, "Dokument 4"),
+                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 5")));
+        LOG.info("Sender HentJournalpostListeResponse med 5 Journalpost.");
+        return hentJournalpostListeResponse;
     }
 
     @WebMethod(action = "http://nav.no/tjeneste/virksomhet/journal/v2/Journal_v2/hentDokumentRequest")
@@ -58,14 +88,14 @@ public class JournalServiceMockImpl implements JournalV2 {
             throw new HentDokumentDokumentIkkeFunnet("DoumentId eller JournalpostId er null.", new DokumentIkkeFunnet());
         } else {
             Path pdfPath = FileSystems.getDefault().getPath("/git/vl-mock/joark-mock/journal/src/main/resources/termin_bekreftelse.pdf");
-            LOG.info("HentDokument: File found at path= " + pdfPath);
+            LOG.info("HentDokument: File found at path=%s ", pdfPath);
             byte[] pdf = new byte[0];
             try {
                 pdf = Files.readAllBytes(pdfPath);
             } catch (IOException e) {
                 LOG.error("Error reading pdf file.", e);
             }
-            LOG.info("HentDokument: Binary pdf file = " + Arrays.toString(pdf));
+            LOG.info("HentDokument: Binary pdf file = %s", pdf);
             hentDokumentResponse.setDokument(pdf);
         }
         return hentDokumentResponse;
@@ -89,5 +119,46 @@ public class JournalServiceMockImpl implements JournalV2 {
     @Override
     public void ping() {
         LOG.info("Ping mottatt og besvart");
+    }
+
+    private Journalpost createJournalpost(String filtype, String variantformat, String tittel) {
+        Journalpost journalpostMedEttDokument = new Journalpost();
+        journalpostMedEttDokument.setJournalpostId(JOURNAL_ID);
+        journalpostMedEttDokument.getDokumentinfoRelasjonListe().add(createDokumentinfoRelasjon(filtype, variantformat, tittel));
+        journalpostMedEttDokument.setSendt(convertToXMLGregorianCalendar(NOW));
+        journalpostMedEttDokument.setMottatt(convertToXMLGregorianCalendar(NOW));
+        return journalpostMedEttDokument;
+    }
+
+    private DokumentinfoRelasjon createDokumentinfoRelasjon(String filtype, String variantformat, String tittel) {
+        DokumentinfoRelasjon dokumentinfoRelasjon = new DokumentinfoRelasjon();
+        JournalfoertDokumentInfo journalfoertDokumentInfo = new JournalfoertDokumentInfo();
+        journalfoertDokumentInfo.setDokumentId(DOKUMENT_ID);
+        Dokumenttyper dokumenttyper = new Dokumenttyper();
+        dokumenttyper.setValue(DOKUMENT_TYPE);
+        journalfoertDokumentInfo.setDokumentType(dokumenttyper);
+        journalfoertDokumentInfo.setTittel(tittel);
+        DokumentInnhold dokumentInnhold = new DokumentInnhold();
+        Arkivfiltyper arkivfiltyper = new Arkivfiltyper();
+        arkivfiltyper.setValue(filtype);
+        dokumentInnhold.setFiltype(arkivfiltyper);
+        Variantformater variantformater = new Variantformater();
+        variantformater.setValue(variantformat);
+        dokumentInnhold.setVariantformat(variantformater);
+        journalfoertDokumentInfo.getBeskriverInnholdListe().add(dokumentInnhold);
+        dokumentinfoRelasjon.setJournalfoertDokument(journalfoertDokumentInfo);
+        return dokumentinfoRelasjon;
+    }
+
+    private static XMLGregorianCalendar convertToXMLGregorianCalendar(LocalDateTime localDateTime) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant()));
+        XMLGregorianCalendar xmlGregorianCalendar = null;
+        try {
+            xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+        } catch (DatatypeConfigurationException e) {
+            LOG.error("", e);
+        }
+        return xmlGregorianCalendar;
     }
 }
