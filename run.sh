2626e80c3da0a20774f15496c7da88c4cae4a7fd
Malvik, Sven <Sven.Malvik@nav.no>
2017-03-24 08:41:13 +0100

    logging to file

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp mock-server/target/mock-server-1.0.2-SNAPSHOT.jar:mock-server/lib/* no.nav.engangsstønad.mock.MockServer &
+java -cp mock-server/target/mock-server-1.0.2-SNAPSHOT.jar:mock-server/lib/* no.nav.engangsstønad.mock.MockServer >> /var/log/mock.log &
