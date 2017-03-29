81585c8016c019e52768c7279d97e34af5a9b71c
Espen Svendsen <espen.svendsen@nav.no>
2017-03-29 14:05:40 +0200

    PK-38818 Lagt inn enkel mock-logikk knyttet til simulert swagger-fagsak

--- joark-mock/journal/src/main/java/no/nav/tjeneste/virksomhet/journal/v2/JournalServiceMockImpl.java
+++ joark-mock/journal/src/main/java/no/nav/tjeneste/virksomhet/journal/v2/JournalServiceMockImpl.java
@@ -1,5 +1,8 @@
 package no.nav.tjeneste.virksomhet.journal.v2;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+
 import no.nav.tjeneste.virksomhet.journal.v2.binding.HentDokumentDokumentIkkeFunnet;
 import no.nav.tjeneste.virksomhet.journal.v2.binding.HentDokumentSikkerhetsbegrensning;
 import no.nav.tjeneste.virksomhet.journal.v2.binding.HentDokumentURLDokumentIkkeFunnet;
@@ -32,15 +35,18 @@ import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
+
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @WebService(name = "Journal_v2", targetNamespace = "http://nav.no/tjeneste/virksomhet/journal/v2")
 public class JournalServiceMockImpl implements JournalV2 {
@@ -54,6 +60,23 @@ public class JournalServiceMockImpl implements JournalV2 {
     private static final String JOURNAL_ID_389425811 = "389425811";
     private static final String DOKUMENT_ID_393893509 = "393893509";
 
+    private static final Map<String, Journalpost> JOURNALPOSTER_PER_FAGSAK = new HashMap<>();
+    private static final Map<String, Journalpost> JOURNALPOST_PER_JOURNAL_ID = new HashMap<>();
+
+    static {
+        // Fagsaksnr settes opp som fnr*100 av simulert Swagger-mottak
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("07078518434") * 100, createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 1", "389425827", "393893532"));
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("07078517802") * 100, createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 1", "389425827", "393893532"));
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("07078517055") * 100, createJournalpost(FILTYPE_XML, VARIANTFORMAT_ARKIV, "Dokument 2", "389425835", "393893544"));
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("05058807410") * 100, createJournalpost(FILTYPE_XML, VARIANTFORMAT_ORIGINAL, "Dokument 3", "389425828", "393893534"));
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("13128807300") * 100, createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ORIGINAL, "Dokument 4", "389425811", DOKUMENT_ID_393893509));
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("03039005009") * 100, createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 5", "389425828", DOKUMENT_ID_393893509));
+        JOURNALPOSTER_PER_FAGSAK.put("" + parseLong("50099414682") * 100, createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 6", JOURNAL_ID_389425811, "393893534"));
+        
+        JOURNALPOSTER_PER_FAGSAK.forEach((saksnr, post) -> JOURNALPOST_PER_JOURNAL_ID.put(post.getJournalpostId(), post));
+    }
+
+
     @WebMethod(action = "http://nav.no/tjeneste/virksomhet/journal/v2/Journal_v2/hentJournalpostListeRequest")
     @WebResult(name = "Response", targetNamespace = "")
     @RequestWrapper(localName = "hentJournalpostListe", targetNamespace = "http://nav.no/tjeneste/virksomhet/journal/v2", className = "no.nav.tjeneste.virksomhet.journal.v2.HentJournalpostListe")
@@ -61,16 +84,20 @@ public class JournalServiceMockImpl implements JournalV2 {
     @Override
     public HentJournalpostListeResponse hentJournalpostListe(@WebParam(name = "Request", targetNamespace = "") HentJournalpostListeRequest request)
             throws HentJournalpostListeSikkerhetsbegrensning {
-        HentJournalpostListeResponse hentJournalpostListeResponse = new HentJournalpostListeResponse();
-        hentJournalpostListeResponse.getJournalpostListe().addAll(Arrays.asList(
-                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 1", "389425827", "393893532"),
-                createJournalpost(FILTYPE_XML, VARIANTFORMAT_ARKIV, "Dokument 2", "389425835", "393893544"),
-                createJournalpost(FILTYPE_XML, VARIANTFORMAT_ORIGINAL, "Dokument 3", "389425828", "393893534"),
-                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ORIGINAL, "Dokument 4", "389425811", DOKUMENT_ID_393893509),
-                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 5", "389425828", DOKUMENT_ID_393893509),
-                createJournalpost(FILTYPE_PDF, VARIANTFORMAT_ARKIV, "Dokument 6", JOURNAL_ID_389425811, "393893534")));
-        LOG.info("Sender HentJournalpostListeResponse med 6 Journalpost.");
-        return hentJournalpostListeResponse;
+        HentJournalpostListeResponse response = new HentJournalpostListeResponse();
+        Optional<String> funnetsaksnr = request.getSakListe().stream().map(sak -> sak.getSakId()).findFirst();
+        if(!funnetsaksnr.isPresent()) {
+            LOG.info("Fant ingen saksnr i request.");
+            return response;
+        }
+        Journalpost journalpost = JOURNALPOSTER_PER_FAGSAK.get(funnetsaksnr.get());
+        if (journalpost == null) {
+            LOG.info("Fant ingen matchende journalpost for saksnr = " + funnetsaksnr.get());
+            return response;
+        }        
+        response.getJournalpostListe().add(journalpost);
+        LOG.info("Sender HentJournalpostListeResponse med 1 Journalpost for saksnr = " + funnetsaksnr.get());
+        return response;
     }
 
     @WebMethod(action = "http://nav.no/tjeneste/virksomhet/journal/v2/Journal_v2/hentDokumentRequest")
@@ -83,11 +110,14 @@ public class JournalServiceMockImpl implements JournalV2 {
         HentDokumentResponse hentDokumentResponse = new HentDokumentResponse();
         if (request.getJournalpostId() == null || request.getDokumentId() == null || request.getJournalpostId().isEmpty() || request.getDokumentId().isEmpty()) {
             throw new HentDokumentDokumentIkkeFunnet("DoumentId eller JournalpostId er null.", new DokumentIkkeFunnet());
-        } else if (JOURNAL_ID_389425811.equals(request.getJournalpostId())) {
+        }
+        Journalpost journalpost = JOURNALPOST_PER_JOURNAL_ID.get(request.getJournalpostId());
+
+        if (journalpost == null) {
             throw new HentDokumentDokumentIkkeFunnet("Dokument ikke funnet for journalpostId :389425811", new DokumentIkkeFunnet());
         } else {
             Path pdfPath;
-            if (DOKUMENT_ID_393893509.equals(request.getDokumentId())) {
+            if (DOKUMENT_ID_393893509.equals(journalpost.getJournalpostId())) {
                 pdfPath = FileSystems.getDefault().getPath("/git/vl-mock/joark-mock/journal/src/main/resources/foreldrepenger_soknad.pdf");
             } else {
                 pdfPath = FileSystems.getDefault().getPath("/git/vl-mock/joark-mock/journal/src/main/resources/termin_bekreftelse.pdf");
@@ -134,7 +164,7 @@ public class JournalServiceMockImpl implements JournalV2 {
         return xmlGregorianCalendar;
     }
 
-    private Journalpost createJournalpost(String filtype, String variantformat, String tittel, String journalpostId, String dokumentId) {
+    private static Journalpost createJournalpost(String filtype, String variantformat, String tittel, String journalpostId, String dokumentId) {
         Journalpost journalpostMedEttDokument = new Journalpost();
         journalpostMedEttDokument.setJournalpostId(journalpostId);
         journalpostMedEttDokument.getDokumentinfoRelasjonListe().add(createDokumentinfoRelasjon(filtype, variantformat, tittel, dokumentId));
@@ -143,7 +173,7 @@ public class JournalServiceMockImpl implements JournalV2 {
         return journalpostMedEttDokument;
     }
 
-    private DokumentinfoRelasjon createDokumentinfoRelasjon(String filtype, String variantformat, String tittel, String dokumentId) {
+    private static DokumentinfoRelasjon createDokumentinfoRelasjon(String filtype, String variantformat, String tittel, String dokumentId) {
         DokumentinfoRelasjon dokumentinfoRelasjon = new DokumentinfoRelasjon();
         JournalfoertDokumentInfo journalfoertDokumentInfo = new JournalfoertDokumentInfo();
         journalfoertDokumentInfo.setDokumentId(dokumentId);
