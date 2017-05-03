13af5f9f57b74c0e271ae3669d60fa090ff6a4d7
Steinar Hjellvik <steinar.hjellvik@nav.no>
2017-05-03 13:59:28 +0200

    Forbedring av PersonService mock test. Har med test av relasjon.

--- tps-mock/person/src/test/java/no/nav/tjeneste/virksomhet/person/v2/PersonServiceMockTest.java
+++ tps-mock/person/src/test/java/no/nav/tjeneste/virksomhet/person/v2/PersonServiceMockTest.java
@@ -22,4 +22,19 @@ public class PersonServiceMockTest {
         assertThat(response.getPerson().getIdent()).isNotNull();
         assertThat(response.getPerson().getIdent().getIdent()).isEqualTo("07078518434");
     }
+
+    @Test
+    public void testHentKjerneinformasjonMedRelasjon() throws HentKjerneinformasjonPersonIkkeFunnet, HentKjerneinformasjonSikkerhetsbegrensning {
+        PersonServiceMockImpl personServiceMock = new PersonServiceMockImpl();
+        HentKjerneinformasjonRequest request = new HentKjerneinformasjonRequest();
+        request.setIdent("11111655830");
+        HentKjerneinformasjonResponse response = personServiceMock.hentKjerneinformasjon(request);
+        assertThat(response).isNotNull();
+        assertThat(response.getPerson()).isNotNull();
+        assertThat(response.getPerson().getIdent()).isNotNull();
+        assertThat(response.getPerson().getIdent().getIdent()).isEqualTo("11111655830");
+        assertThat(response.getPerson().getHarFraRolleI()).isNotNull();
+        assertThat(response.getPerson().getHarFraRolleI()).isNotEmpty();
+        assertThat(response.getPerson().getHarFraRolleI().get(0).getTilPerson()).isNotNull();
+    }
 }
