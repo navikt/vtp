d0d017f38e5a0a54d088e1f93a947d6c4e92c7ec
Iver Stubdal <iver.stubdal@nav.no>
2017-06-08 10:53:06 +0200

    PK-42567 mocker ABAC DENY for 2 kode 6/7 brukere

--- pdp-mock/src/main/java/no/nav/abac/pdp/PdpMock.java
+++ pdp-mock/src/main/java/no/nav/abac/pdp/PdpMock.java
@@ -1,5 +1,7 @@
 package no.nav.abac.pdp;
 
+import org.apache.commons.io.IOUtils;
+
 import javax.mail.util.ByteArrayDataSource;
 import javax.activation.DataSource;
 import javax.xml.ws.BindingType;
@@ -7,6 +9,7 @@ import javax.xml.ws.Provider;
 import javax.xml.ws.ServiceMode;
 import javax.xml.ws.WebServiceProvider;
 import javax.xml.ws.http.HTTPBinding;
+import java.io.IOException;
 
 @WebServiceProvider
 @ServiceMode(value = javax.xml.ws.Service.Mode.MESSAGE)
@@ -15,9 +18,24 @@ public class PdpMock implements Provider<DataSource> {
 
     @Override
     public DataSource invoke(DataSource request) {
+        String xacmlRequest="";
+        try {
+            xacmlRequest = IOUtils.toString(request.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(xacmlRequest.contains("{\"AttributeId\":\"no.nav.abac.attributter.resource.felles.person.fnr\",\"Value\":\"03031751364\"}")||
+        xacmlRequest.contains("{\"AttributeId\":\"no.nav.abac.attributter.resource.felles.person.fnr\",\"Value\":\"08059003306\"}")){
+            return new ByteArrayDataSource(buildDenyResponse().getBytes(),"application/json");
+        }
         return new ByteArrayDataSource(buildPermitResponse().getBytes(),"application/json");
     }
 
+    private String buildDenyResponse()   {
+        return " { \"Response\" : {\"Decision\" : \"Deny\",\"Status\" : {\"StatusCode\" : {\"Value\" : " +
+            "\"urn:oasis:names:tc:xacml:1.0:status:ok\",\"StatusCode\" : {\"Value\" : " +
+            "\"urn:oasis:names:tc:xacml:1.0:status:ok\"}}}}}";
+}
     private String buildPermitResponse() {
         return " { \"Response\" : {\"Decision\" : \"Permit\",\"Status\" : {\"StatusCode\" : {\"Value\" : " +
                 "\"urn:oasis:names:tc:xacml:1.0:status:ok\",\"StatusCode\" : {\"Value\" : " +
