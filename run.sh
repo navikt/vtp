fdb08bc84ca05f66ffb406b6c31f60b5412ad7ba
Malvik, Sven <Sven.Malvik@nav.no>
2017-06-30 10:50:13 +0200

    NOJIRA lagt til env variabler

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export mock_keystore=/home/deployer/keystore.jks
+export modigcerts=false
 cd /var/log/apps/mock
 java -cp  /home/deployer/mock-server/target/mock-server-1.0.4-SNAPSHOT.jar:/home/deployer/mock-server/lib/* no.nav.engangsstønad.mock.MockServer >> console.log &
