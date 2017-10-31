89366bcbcafb9f98afb439947005a637103a4e9f
Ola Løvholm <ola.lovholm@nav.no>
2017-10-31 20:01:34 +0100

    Landkoder lagt til i adresser.

--- tps-mock/modell/src/main/java/no/nav/tjeneste/virksomhet/person/v3/modell/PersonBygger.java
+++ tps-mock/modell/src/main/java/no/nav/tjeneste/virksomhet/person/v3/modell/PersonBygger.java
@@ -149,6 +149,12 @@ public class PersonBygger {
         postadressetyper.setValue(this.gjeldendeAdresseType);
         bruker.setGjeldendePostadressetype(postadressetyper);
 
+        Landkoder landkoderNorge = new Landkoder();
+        landkoderNorge.setValue("NOR");
+
+        Landkoder landkoderUtland = new Landkoder();
+        landkoderUtland.setValue("USA");
+
 
         if(this.gjeldendeAdresseType.equalsIgnoreCase("UKJENT_ADRESSE")){
 
@@ -159,6 +165,10 @@ public class PersonBygger {
             gateadresse.setGatenummer(15);
             gateadresse.setHusbokstav("B");
             gateadresse.setHusnummer(10);
+            gateadresse.setLandkode(landkoderNorge);
+            Postnummer postnummer = new Postnummer();
+            postnummer.setValue("2500");
+            gateadresse.setPoststed(postnummer);
             adresse.setStrukturertAdresse(gateadresse);
             bruker.setBostedsadresse(adresse);
         } else if (gjeldendeAdresseType.equalsIgnoreCase("POSTADRESSE") || gjeldendeAdresseType.toLowerCase().contains("midlertidig")){
@@ -168,6 +178,7 @@ public class PersonBygger {
             ustrukturertAdresse.setAdresselinje2("Gateadresse 2");
             ustrukturertAdresse.setAdresselinje3("Rislunch");
             ustrukturertAdresse.setAdresselinje4("2500 Tynset");
+            ustrukturertAdresse.setLandkode(landkoderNorge);
             postadresse.setUstrukturertAdresse(ustrukturertAdresse);
             bruker.setPostadresse(postadresse);
         }
@@ -179,6 +190,7 @@ public class PersonBygger {
             ustrukturertAdresse.setAdresselinje2("Central district");
             ustrukturertAdresse.setAdresselinje3("Washington DC");
             ustrukturertAdresse.setAdresselinje4("USA");
+            ustrukturertAdresse.setLandkode(landkoderUtland);
             postadresse.setUstrukturertAdresse(ustrukturertAdresse);
             bruker.setMidlertidigPostadresse(postadresse);
         }
@@ -188,6 +200,7 @@ public class PersonBygger {
              PostboksadresseNorsk strukturertAdresse = new PostboksadresseNorsk();
              strukturertAdresse.setPostboksanlegg("Vinsjan ved kaia");
              strukturertAdresse.setPostboksnummer("1337");
+             strukturertAdresse.setLandkode(landkoderUtland);
              Postnummer postnummer = new Postnummer();
              postnummer.setValue("2500");
              strukturertAdresse.setPoststed(postnummer);
