141fdfb1ce88cc0704005d8c2fa14394accdbf78
Malvik, Sven <Sven.Malvik@nav.no>
2017-12-06 11:30:01 +0100

    NOJIRA gitignore

--- mock-server/src/main/java/no/nav/engangsstønad/mock/MockServer.java
+++ mock-server/src/main/java/no/nav/engangsstønad/mock/MockServer.java
@@ -41,7 +41,7 @@ import java.lang.reflect.Method;
 
 public class MockServer {
 
-    private static final int HTTP_PORT = 7999;
+    private static final int HTTP_PORT = 8080;
     private static final int HTTPS_PORT = 8443;
     private static final String HTTP_HOST = "0.0.0.0";
     private static Server server;
@@ -114,6 +114,7 @@ public class MockServer {
         ServerConnector sslConnector = new ServerConnector(server,
                 new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(https));
         sslConnector.setPort(HTTPS_PORT);
+        sslConnector.setHost(HTTP_HOST);
         server.setConnectors(new Connector[] { connector, sslConnector });
     }
 
