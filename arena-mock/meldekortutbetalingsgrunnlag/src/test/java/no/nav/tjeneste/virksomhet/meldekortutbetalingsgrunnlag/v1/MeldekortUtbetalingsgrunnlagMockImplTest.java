79bbbf62a07f6f00fb36dbc97da410ee75207cb4
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2018-03-15 20:22:11 +0100

    PKTERMITT-110 første utkast til mock for meldekortUtbetalingsgrunnlag

---
+++ arena-mock/meldekortutbetalingsgrunnlag/src/test/java/no/nav/tjeneste/virksomhet/meldekortutbetalingsgrunnlag/v1/MeldekortUtbetalingsgrunnlagMockImplTest.java
@@ -0,0 +1,38 @@
+package no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1;
+
+
+import no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.binding.FinnMeldekortUtbetalingsgrunnlagListeAktoerIkkeFunnet;
+import no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.binding.FinnMeldekortUtbetalingsgrunnlagListeSikkerhetsbegrensning;
+import no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.binding.FinnMeldekortUtbetalingsgrunnlagListeUgyldigInput;
+import no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.informasjon.AktoerId;
+import no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.meldinger.FinnMeldekortUtbetalingsgrunnlagListeRequest;
+import no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.informasjon.ObjectFactory;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MeldekortUtbetalingsgrunnlagMockImplTest {
+    private final String IDENT = "26048121054";
+    private ObjectFactory objectFactory = new ObjectFactory();
+
+    @Ignore
+    @Test
+    public void testFinnMeldekortUtbetalingsgrunnlagListeResponse() throws FinnMeldekortUtbetalingsgrunnlagListeSikkerhetsbegrensning, FinnMeldekortUtbetalingsgrunnlagListeAktoerIkkeFunnet, FinnMeldekortUtbetalingsgrunnlagListeUgyldigInput {
+
+        MeldekortUtbetalingsgrunnlagMockImpl hentYtelse = new MeldekortUtbetalingsgrunnlagMockImpl();
+        FinnMeldekortUtbetalingsgrunnlagListeRequest request = new FinnMeldekortUtbetalingsgrunnlagListeRequest();
+        AktoerId aktoerId = objectFactory.createAktoerId();
+        aktoerId.setAktoerId(IDENT);
+        request.setIdent(aktoerId);
+
+        try {
+            no.nav.tjeneste.virksomhet.meldekortutbetalingsgrunnlag.v1.meldinger.FinnMeldekortUtbetalingsgrunnlagListeResponse response =
+                    hentYtelse.finnMeldekortUtbetalingsgrunnlagListe(request);
+            assertThat(response).isNotNull();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
