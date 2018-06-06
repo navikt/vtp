5fc8185dbfc1d761b71560b7d97a75e756d98e5b
Frode Carlsen <frode.odde.carlsen@nav.no>
2018-06-06 14:08:26 +0200

    FIX run-java.sh skrivefeil

--- run-java.sh
+++ run-java.sh
@@ -2,4 +2,4 @@
 
 mkdir logs
 export APP_LOG_HOME=./logs
-exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* no.nav.vedtdak.mock.local.MockServer
+exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* no.nav.vedtak.mock.local.MockServer
