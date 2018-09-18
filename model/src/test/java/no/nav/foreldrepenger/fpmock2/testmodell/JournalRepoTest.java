d18d70ede6c2ae952b0d43366af172bb2bf61826
Ola Løvholm <ola.lovholm@nav.no>
2018-09-18 19:44:45 +0200

    PFP-931: Test på avsenderFnr

--- model/src/test/java/no/nav/foreldrepenger/fpmock2/testmodell/JournalRepoTest.java
+++ model/src/test/java/no/nav/foreldrepenger/fpmock2/testmodell/JournalRepoTest.java
@@ -4,8 +4,8 @@ package no.nav.foreldrepenger.fpmock2.testmodell;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.DokumentModell;
@@ -16,7 +16,7 @@ import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.JournalRepositoryImpl;
 public class JournalRepoTest {
 
     @Test
-    public void skalHenteJournalposterPåSaksId(){
+    public void skalFinneJournalposterPåSaksid(){
 
         String sakId = "12381";
 
@@ -43,7 +43,31 @@ public class JournalRepoTest {
     }
 
     @Test
-    public void skalSetteJournalpostId() {
+    public void skalFinneJournalposterPåAvsenderFnr(){
+        JournalRepository journalRepository = new JournalRepositoryImpl();
+
+        String avsenderFnrSøker = "01020304056";
+        String avsenderFnrIkkeSøker = "99999999999";
+
+        JournalpostModell journalpostModellSøker_1 = new JournalpostModell();
+        journalpostModellSøker_1.setAvsenderFnr(avsenderFnrSøker);
+        journalRepository.leggTilJournalpost(journalpostModellSøker_1);
+
+        JournalpostModell journalpostModellSøker_2 = new JournalpostModell();
+        journalpostModellSøker_2.setAvsenderFnr(avsenderFnrSøker);
+        journalRepository.leggTilJournalpost(journalpostModellSøker_2);
+
+        JournalpostModell journalpostModellIkkeSøker_1 = new JournalpostModell();
+        journalpostModellIkkeSøker_1.setAvsenderFnr(avsenderFnrIkkeSøker);
+        journalRepository.leggTilJournalpost(journalpostModellIkkeSøker_1);
+
+        List<JournalpostModell> journalpostModells = journalRepository.finnJournalposterMedFnr(avsenderFnrSøker);
+        assertThat(journalpostModells).size().isEqualTo(2);
+
+    }
+
+    @Test
+    public void skalSetteJournalpostIdVedLagring() {
         JournalRepository journalRepository = new JournalRepositoryImpl();
 
         JournalpostModell journalpostModell = new JournalpostModell();
@@ -51,9 +75,11 @@ public class JournalRepoTest {
         journalpostModell.setAvsenderFnr("00115522447");
         String journalpostId = journalRepository.leggTilJournalpost(journalpostModell);
 
-        Assert.assertTrue(journalRepository.finnJournalpostMedJournalpostId(journalpostId).get().getId().equals(journalpostId));
-
+        Optional<JournalpostModell> resultatModell = journalRepository.finnJournalpostMedJournalpostId(journalpostId);
+        assertThat(resultatModell.isPresent());
+        assertThat(resultatModell.get().getId()).isEqualTo(journalpostId);
     }
+
 
 
 }
