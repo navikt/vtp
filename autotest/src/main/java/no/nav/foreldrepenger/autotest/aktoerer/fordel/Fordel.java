583ff91c00e0f2ef8bb67b67c87add4f9392cc4f
Morten Svendsen <Morten.Svendsen@nav.no>
2018-09-19 09:44:56 +0200

    la til aktør for fordeling

---
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/aktoerer/fordel/Fordel.java
@@ -0,0 +1,30 @@
+package no.nav.foreldrepenger.autotest.aktoerer.fordel;
+
+import no.nav.foreldrepenger.autotest.aktoerer.Aktoer;
+
+public class Fordel extends Aktoer{
+    
+    /*
+     * Klienter
+     */
+    
+    public Fordel() {
+        
+    }
+    
+    
+    /*
+     * Sender inn søkand og returnerer saksinformasjon
+     */
+    public Object sendInnSøknad(Object object) {
+        return null;
+    }
+    
+    /*
+     * Sender inn søknad og returnerer saksinformasjon
+     */
+    public Object sendInnPapirsøkand(Object object) {
+        return null;
+    }
+    
+}
