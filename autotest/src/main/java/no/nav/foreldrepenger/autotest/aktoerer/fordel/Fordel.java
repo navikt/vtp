3e402f1af6a30c1e54fce1408968cba89e3e7248
Morten Svendsen <Morten.Svendsen@nav.no>
2018-09-24 12:18:54 +0200

    work on fordel and test

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/aktoerer/fordel/Fordel.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/aktoerer/fordel/Fordel.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.autotest.aktoerer.fordel;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Base64;
 
 import no.nav.foreldrepenger.autotest.aktoerer.Aktoer;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.fordel.FordelKlient;
@@ -10,6 +11,10 @@ import no.nav.foreldrepenger.autotest.klienter.fpsak.fordel.dto.JournalpostKnytt
 import no.nav.foreldrepenger.autotest.klienter.fpsak.fordel.dto.JournalpostMottak;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.fordel.dto.OpprettSak;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.fordel.dto.Saksnummer;
+import no.nav.foreldrepenger.fpmock2.testmodell.dokument.JournalpostModellGenerator;
+import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.JournalpostModell;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.JournalRepository;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.JournalRepositoryImpl;
 
 public class Fordel extends Aktoer{
     
@@ -27,6 +32,12 @@ public class Fordel extends Aktoer{
      * Sender inn søkand og returnerer saksinformasjon
      */
     public long sendInnSøknad(Object søknad) {
+        String xml = "</xml>";
+        String fnr = "105891918";
+        JournalpostModell journalpostModell = JournalpostModellGenerator.foreldrepengeSøknadFødselJournalpost(xml, fnr);
+        JournalRepository journalRepository = JournalRepositoryImpl.getInstance();
+        journalRepository.leggTilJournalpost(journalpostModell);
+        
         return -1;
     }
     
@@ -46,6 +57,13 @@ public class Fordel extends Aktoer{
     }
     
     public long sendInnInntektsmelding(String journalpostId, String behandlingstemaOffisiellKode, String dokumentTypeIdOffisiellKode, String aktørId) throws IOException {
+        return sendInnJournalpost(null, journalpostId, behandlingstemaOffisiellKode, dokumentTypeIdOffisiellKode, aktørId);
+    }
+    
+    /*
+     * Sender inn journalpost og returnerer saksnummer
+     */
+    private long sendInnJournalpost(String xml, String journalpostId, String behandlingstemaOffisiellKode, String dokumentTypeIdOffisiellKode, String aktørId) throws IOException {
         OpprettSak journalpost = new OpprettSak(journalpostId, behandlingstemaOffisiellKode, aktørId);
         Saksnummer saksnummer = fordelKlient.fagsakOpprett(journalpost);
         
@@ -56,21 +74,14 @@ public class Fordel extends Aktoer{
         
         JournalpostMottak journalpostMottak = new JournalpostMottak("" + saksnummer.saksnummer, journalpostId, LocalDate.now(), behandlingstemaOffisiellKode);
         journalpostMottak.dokumentTypeIdOffisiellKode = dokumentTypeIdOffisiellKode;
-        //String xmlText = readFile();
-        //journalpostMottak.payloadXml = new String(Base64.getUrlEncoder().withoutPadding().encode(xmlText.getBytes()));
-        //journalpostMottak.payloadLength = xmlText.length();
+        
+        if(null != xml) {
+            journalpostMottak.payloadXml = new String(Base64.getUrlEncoder().withoutPadding().encode(xml.getBytes()));
+            journalpostMottak.payloadLength = xml.length();
+        }
+        
         fordelKlient.journalpost(journalpostMottak);
         
         return saksnummer.saksnummer;
     }
-
-
-    // DELETEME temp til vi får xml objekter
-    /*
-    private String readFile() throws IOException {
-        byte[] encoded = Files.readAllBytes(Paths.get("test.xml"));
-        return new String(encoded, Charset.defaultCharset());
-    }
-    */
-    
 }
--- autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/eksempler/OverstyringAvGradering.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/eksempler/OverstyringAvGradering.java
@@ -3,10 +3,21 @@ package no.nav.foreldrepenger.autotest.tests.eksempler;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.overstyr.OverstyrUttaksperioder;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.kodeverk.dto.Kode;
 import no.nav.foreldrepenger.autotest.tests.FpsakTestBase;
+import no.nav.foreldrepenger.fpmock2.testmodell.dokument.JournalpostModellGenerator;
+import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.JournalpostModell;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.JournalRepository;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.TestscenarioImpl;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.BasisdataProviderFileImpl;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.JournalRepositoryImpl;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioRepositoryImpl;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioTemplateRepositoryImpl;
 
 public class OverstyringAvGradering extends FpsakTestBase{
 
     public void skalKunneOverstyreGradering() throws Exception {
+        TestscenarioRepositoryImpl instance = TestscenarioRepositoryImpl.getInstance(BasisdataProviderFileImpl.getInstance());
+        TestscenarioImpl testscenario = instance.opprettTestscenario(TestscenarioTemplateRepositoryImpl.getInstance().finn("50"));
+        
         fordel.erLoggetInnUtenRolle();
         /*
         Søkand søkand = SøknadBuilder.nyFødselsøknad();
