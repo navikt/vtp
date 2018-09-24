e9da6f4ca2e25a39ffb92449ad9135a92a3a58b8
Morten Svendsen <Morten.Svendsen@nav.no>
2018-09-24 12:54:56 +0200

    cleanup

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/ProofOfConceptTest.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/ProofOfConceptTest.java
@@ -2,15 +2,7 @@ package no.nav.foreldrepenger.autotest.tests;
 
 import org.junit.jupiter.api.Test;
 
-import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.erketyper.ForeldrepengesoknadXmlErketyper;
-import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.soeknad.ForeldrepengesoknadBuilder;
-import no.nav.foreldrepenger.fpmock2.testmodell.dokument.JournalpostModellGenerator;
-import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.JournalpostModell;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.JournalRepository;
 import no.nav.foreldrepenger.fpmock2.testmodell.repo.TestscenarioImpl;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.BasisdataProviderFileImpl;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.JournalRepositoryImpl;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioRepositoryImpl;
 import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioTemplateRepositoryImpl;
 import no.nav.vedtak.felles.xml.soeknad.v1.Soeknad;
 
--- autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/eksempler/OverstyringAvGradering.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/eksempler/OverstyringAvGradering.java
@@ -3,13 +3,7 @@ package no.nav.foreldrepenger.autotest.tests.eksempler;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.overstyr.OverstyrUttaksperioder;
 import no.nav.foreldrepenger.autotest.klienter.fpsak.kodeverk.dto.Kode;
 import no.nav.foreldrepenger.autotest.tests.FpsakTestBase;
-import no.nav.foreldrepenger.fpmock2.testmodell.dokument.JournalpostModellGenerator;
-import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.JournalpostModell;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.JournalRepository;
 import no.nav.foreldrepenger.fpmock2.testmodell.repo.TestscenarioImpl;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.BasisdataProviderFileImpl;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.JournalRepositoryImpl;
-import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioRepositoryImpl;
 import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioTemplateRepositoryImpl;
 import no.nav.vedtak.felles.xml.soeknad.v1.Soeknad;
 
