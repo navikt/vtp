9b0c29e63798cf0c3f2c0aee626c60d4e4b69043
Morten Svendsen <Morten.Svendsen@nav.no>
2018-10-08 15:35:19 +0200

    la til starten av medlemskapstesten

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/engangsstonad/Medlemskap.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/tests/engangsstonad/Medlemskap.java
@@ -1,5 +1,36 @@
 package no.nav.foreldrepenger.autotest.tests.engangsstonad;
 
+import java.time.LocalDate;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.VurderManglendeFodselBekreftelse;
+import no.nav.foreldrepenger.autotest.klienter.fpsak.behandlinger.dto.aksjonspunktbekreftelse.avklarfakta.AvklarFaktaTillegsopplysningerBekreftelse;
+import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.koder.DokumenttypeId;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.TestscenarioImpl;
+import no.nav.foreldrepenger.fpmock2.testmodell.repo.impl.TestscenarioTemplateRepositoryImpl;
+import no.nav.vedtak.felles.xml.soeknad.v1.Soeknad;
+
 public class Medlemskap extends EngangsstonadTestBase{
 
+    public void behandleFødselMorErUtvandret() throws Exception{
+        TestscenarioImpl testscenario = testscenarioRepository.opprettTestscenario(TestscenarioTemplateRepositoryImpl.getInstance().finn("51"));
+        Soeknad søknad = foreldrepengeSøknadErketyper.fodselfunnetstedUttakKunMorEngangstonad(testscenario.getPersonopplysninger().getSøker().getAktørIdent());
+
+        fordel.erLoggetInnMedRolle("Saksbehandler");
+        long saksnummer = fordel.sendInnSøknad(søknad, testscenario, DokumenttypeId.FOEDSELSSOKNAD_ENGANGSSTONAD);
+        System.out.println("Saksnummer: " + saksnummer);
+        
+        saksbehandler.erLoggetInnMedRolle("Saksbehandler");
+        saksbehandler.hentFagsak(saksnummer);
+        saksbehandler.bekreftAksjonspunktBekreftelse(AvklarFaktaTillegsopplysningerBekreftelse.class);
+        
+        saksbehandler.hentAksjonspunktbekreftelse(VurderManglendeFodselBekreftelse.class)
+            .bekreftDokumentasjonForeligger(1, LocalDate.now().minusMonths(1));
+        saksbehandler.bekreftAksjonspunktBekreftelse(VurderManglendeFodselBekreftelse.class);
+        
+        //Skal nå ha vurdering på medlemskap
+    }
 }
