f748eb63125c40aef86d04becc48508bf1d02e26
Martin Sommervold <martin.sommervold@nav.no>
2018-10-09 14:34:37 +0200

    PFP-1630 fjerner ubrukt klasse

--- server/src/main/java/no/nav/foreldrepenger/fpmock2/server/api/journalforing/TilgjengeligeForeldrepengesoknadErketyperEnum.java
+++
@@ -1,6 +0,0 @@
-package no.nav.foreldrepenger.fpmock2.server.api.journalforing;
-
-public enum TilgjengeligeForeldrepengesoknadErketyperEnum {
-    foreldrepengesoknadTermindatoKunMorMedInntektsmelding,
-    engangsttonadFodselFunnetStedKunMor
-}
