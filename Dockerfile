617ed442d0b03de8d268f658babf857438a62cff
Martin Sommervold <martin.sommervold@nav.no>
2018-10-11 00:07:55 +0200

    NOJIRA sendrer tilbake til 8060, spesifiserer scenario-folder i run-java.sh.

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ ARG JAR_FILE
 RUN mkdir /app/lib
 COPY server/lib/*.jar /app/lib/
 COPY model/scenarios/ /app/model/scenarios/
+RUN chmod -R 777 /app/model/scenarios/
 
 COPY server/src/main/resources/logback.xml logback.xml
 COPY server/target/server*.jar app.jar
--- run-java.sh
+++ run-java.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* -Dlogback.configurationFile=logback.xml no.nav.foreldrepenger.fpmock2.server.MockServer
+exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* -Dlogback.configurationFile=logback.xml -Dscenarios.dir="/app/model/scenarios/" no.nav.foreldrepenger.fpmock2.server.MockServer
