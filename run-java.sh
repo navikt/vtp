7f4a8b8ef85255b8be963a87fcf3ff692303b515
Martin Sommervold <martin.sommervold@nav.no>
2018-10-12 10:52:15 +0200

    NOJIRA longshot

--- run-java.sh
+++ run-java.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* -Dlogback.configurationFile=logback.xml no.nav.foreldrepenger.fpmock2.server.MockServer
+exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* -Dlogback.configurationFile=logback.xml -Dfile.encoding=UTF8 no.nav.foreldrepenger.fpmock2.server.MockServer
