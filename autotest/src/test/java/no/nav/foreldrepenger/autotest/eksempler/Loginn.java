2ba2060ea636fba8a5f6a15c131f599d9e1f4bba
Morten Svendsen <Morten.Svendsen@nav.no>
2018-10-12 10:48:57 +0200

    fikset test

--- autotest/src/test/java/no/nav/foreldrepenger/autotest/eksempler/Loginn.java
+++ autotest/src/test/java/no/nav/foreldrepenger/autotest/eksempler/Loginn.java
@@ -3,12 +3,15 @@ package no.nav.foreldrepenger.autotest.eksempler;
 import java.io.UnsupportedEncodingException;
 
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import no.nav.foreldrepenger.autotest.FpsakTestBase;
 
 @Tag("eksempel")
 public class Loginn extends FpsakTestBase{
     
+    @Test
+    @Tag("utvikling")
     void loginnUtenRolle() throws UnsupportedEncodingException {
         saksbehandler.erLoggetInnUtenRolle();
     }
