8ada1bae9b3555f70c90821cce0b76d070e4a6c6
Morten Svendsen <Morten.Svendsen@nav.no>
2018-10-24 13:05:11 +0200

    uopped version of maven shurefire and added ability to exclude tests + defeult profile

--- autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/engangsstonad/Innsyn.java
+++ autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/engangsstonad/Innsyn.java
@@ -13,6 +13,7 @@ import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.soekn
 import no.nav.foreldrepenger.fpmock2.server.api.scenario.TestscenarioDto;
 import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.koder.DokumenttypeId;
 
+@Tag("smoke")
 @Tag("engangsstonad")
 public class Innsyn extends EngangsstonadTestBase{
 
