214d0a7e054859310b28cc1aff1e7a3e144b6366
Morten Svendsen <Morten.Svendsen@nav.no>
2018-11-15 11:45:07 +0100

    mulighet for å generere feilmelding ved vent

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/util/vent/Vent.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/util/vent/Vent.java
@@ -6,11 +6,21 @@ import java.util.concurrent.Callable;
 public class Vent {
     
     public static void til(Callable<Boolean> callable, int timeoutInSeconds, String failReason) throws Exception {
+        til(callable, timeoutInSeconds, new Callable<String>() {
+            
+            @Override
+            public String call() throws Exception {
+                return failReason;
+            }
+        });
+    }
+    
+    public static void til(Callable<Boolean> callable, int timeoutInSeconds, Callable<String> errorMessageProducer) throws Exception {
         LocalDateTime end = LocalDateTime.now().plusSeconds(timeoutInSeconds);
 
         while (!callable.call()) {
             if (LocalDateTime.now().isAfter(end)) {
-                throw new RuntimeException(String.format("Async venting timet ut etter %s sekunder fordi: %s", timeoutInSeconds, failReason));
+                throw new RuntimeException(String.format("Async venting timet ut etter %s sekunder fordi: %s", timeoutInSeconds, errorMessageProducer.call()));
             }
             Thread.sleep(100);
         }
