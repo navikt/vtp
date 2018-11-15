69dc951e0bfff691a1af0a51a09a3161d497b2dd
Morten Svendsen <Morten.Svendsen@nav.no>
2018-11-15 12:12:34 +0100

    feilaktig ignorerte tester. mulig regresjon

--- autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/engangsstonad/Innsyn.java
+++ autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/engangsstonad/Innsyn.java
@@ -2,7 +2,6 @@ package no.nav.foreldrepenger.autotest.foreldrepenger.engangsstonad;
 
 import java.time.LocalDate;
 
-import org.junit.Ignore;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -19,7 +18,6 @@ import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.koder.Dokumentty
 public class Innsyn extends EngangsstonadTestBase{
 
     @Test
-    @Ignore
     public void behandleInnsynMorGodkjent() throws Exception {
         TestscenarioDto testscenario = opprettScenario("50");
         ForeldrepengesoknadBuilder søknad = foreldrepengeSøknadErketyper.fodselfunnetstedUttakKunMorEngangstonad(testscenario.getPersonopplysninger().getSøkerAktørIdent());
@@ -50,7 +48,6 @@ public class Innsyn extends EngangsstonadTestBase{
     }
     
     @Test
-    @Ignore
     public void behandleInnsynMorAvvist() throws Exception {
         TestscenarioDto testscenario = opprettScenario("50");
         ForeldrepengesoknadBuilder søknad = foreldrepengeSøknadErketyper.fodselfunnetstedUttakKunMorEngangstonad(testscenario.getPersonopplysninger().getSøkerAktørIdent());
@@ -80,7 +77,6 @@ public class Innsyn extends EngangsstonadTestBase{
     }
     
     @Test
-    @Ignore
     public void behandleInnsynFarAvvist() throws Exception {
         TestscenarioDto testscenario = opprettScenario("61");
         ForeldrepengesoknadBuilder søknad = foreldrepengeSøknadErketyper.terminFarEngangstonad(testscenario.getPersonopplysninger().getSøkerAktørIdent());
