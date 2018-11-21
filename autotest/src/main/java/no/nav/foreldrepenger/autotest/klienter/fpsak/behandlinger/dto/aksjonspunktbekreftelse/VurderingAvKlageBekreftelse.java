ee4f3001240419ee174f63fd5df15c249fe7df11
Mari Vatn <mari.vatn@nav.no>
2018-11-21 12:54:45 +0100

    Fjerner avvis

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/VurderingAvKlageBekreftelse.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/behandlinger/dto/aksjonspunktbekreftelse/VurderingAvKlageBekreftelse.java
@@ -8,19 +8,17 @@ import java.time.LocalDate;
 public abstract class VurderingAvKlageBekreftelse extends AksjonspunktBekreftelse {
 
     private static final String VURDERING_STADFEST = "STADFESTE_YTELSESVEDTAK";
-    //private static final String VURDERING_AVVIS = "AVVIS_KLAGE";
     private static final String VURDERING_MEDHOLD = "MEDHOLD_I_KLAGE";
     
     protected String klageVurdering;
     protected String klageMedholdArsak;
-    protected String klageAvvistArsak;
     protected String klageVurderingOmgjoer;
     protected String fritekstTilBrev;
     protected LocalDate vedtaksdatoPaklagdBehandling;
     
     public VurderingAvKlageBekreftelse(Fagsak fagsak, Behandling behandling) {
         super(fagsak, behandling);
-        this.vedtaksdatoPaklagdBehandling = LocalDate.now(); //TODO: hent dato til påklagd vedtak
+        this.vedtaksdatoPaklagdBehandling = LocalDate.now(); //TODO: hent dato til påklagd vedtak. Nødvendig?
     }
 
     // Omgjør vedtaket
@@ -29,12 +27,6 @@ public abstract class VurderingAvKlageBekreftelse extends AksjonspunktBekreftels
         klageMedholdArsak = årsak;
         return this;
     }
-
-    /*public VurderingAvKlageBekreftelse bekreftAvvist(String årsak) {
-        klageVurdering  = VURDERING_AVVIS;
-        klageAvvistArsak = årsak;
-        return this;
-    }*/
 
     // oppretthold vedtaket
     public VurderingAvKlageBekreftelse bekreftStadfestet() {
