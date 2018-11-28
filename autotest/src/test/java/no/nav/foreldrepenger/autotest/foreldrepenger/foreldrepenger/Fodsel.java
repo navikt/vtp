2e2b5a404bc7ad3d3d3f487f7b75915c3aadd327
Anders Palfi <anders.palfi@nav.no>
2018-11-28 12:33:03 +0100

    NOJIRA fikset kompileringsfeil

--- autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/foreldrepenger/Fodsel.java
+++ autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/foreldrepenger/Fodsel.java
@@ -11,6 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
