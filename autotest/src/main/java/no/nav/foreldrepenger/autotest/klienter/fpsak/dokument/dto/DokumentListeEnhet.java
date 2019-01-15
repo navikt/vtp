33eac1e9294979340068878ac864eaabc38585de
Morten Svendsen <Morten.Svendsen@nav.no>
2019-01-15 15:38:22 +0100

    fikset DokumentListEnhet

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/dokument/dto/DokumentListeEnhet.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/klienter/fpsak/dokument/dto/DokumentListeEnhet.java
@@ -2,6 +2,9 @@ package no.nav.foreldrepenger.autotest.klienter.fpsak.dokument.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class DokumentListeEnhet {
     protected String dokumentId;
     protected String gjelderFor;
