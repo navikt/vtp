a783d9f12b44a485da39555749b1ccdf1e8a31d6
Ola Løvholm <ola.lovholm@nav.no>
2019-01-16 15:42:56 +0100

    PFP-4437: trekker ned ny klient og legger til i app/lib

--- resources/fpsakmqmock/getmqclients.sh
+++ resources/fpsakmqmock/getmqclients.sh
@@ -10,8 +10,16 @@ oversion=$(grep "<version>" maven-metadata-o.xml | sed 's/<[^>]*>//g'| awk '{ pr
 echo "Siste versjon okonomistotte er $oversion"
 rm maven-metadata-o.xml
 
+curl http://maven.adeo.no/nexus/service/local/repositories/m2internal/content/no/nav/vedtak/felles/integrasjon/felles-integrasjon-jms/maven-metadata.xml -o maven-metadata-jms.xml -s
+jmsversion=$(grep "<version>" maven-metadata-jms.xml | sed 's/<[^>]*>//g'| awk '{ print $1 }' | tail -n 1)
+echo "Siste versjon felles-integrasjon-jms er $jmsversion"
+rm maven-metadata-jms.xml
+
 echo "Henter sak og behandling..."
 wget "http://maven.adeo.no/nexus/service/local/repositories/m2internal/content/no/nav/vedtak/felles/integrasjon/sakogbehandling-klient/$sbversion/sakogbehandling-klient-$sbversion-tests.jar" -O sakogbehandling-k$
 
 echo "Henter okonomi..."
 wget "http://maven.adeo.no/nexus/service/local/repositories/m2internal/content/no/nav/vedtak/felles/integrasjon/okonomistotte-jms/$oversion/okonomistotte-jms-$oversion-tests.jar" -O okonomi.jar
+
+echo "Henter jms"
+wget "http://maven.adeo.no/nexus/service/local/repositories/m2internal/content/no/nav/vedtak/felles/integrasjon/felles-integrasjon-jms/$jmsversion/felles-integrasjon-jms-$jmsversion-tests.jar" -O felles-integrasjon-jms.jar
