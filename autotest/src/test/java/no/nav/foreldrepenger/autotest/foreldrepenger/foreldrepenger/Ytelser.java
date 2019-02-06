c1a8eb3598d16a13c6ae904c37ee9a162c67c283
Morten Svendsen <Morten.Svendsen@nav.no>
2019-02-06 15:00:55 +0100

    startet på tester

---
+++ autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/foreldrepenger/Ytelser.java
@@ -0,0 +1,29 @@
+package no.nav.foreldrepenger.autotest.foreldrepenger.foreldrepenger;
+
+import org.junit.jupiter.api.Tag;
+
+@Tag("smoke")
+@Tag("foreldrepenger")
+public class Ytelser extends ForeldrepengerTestBase {
+
+    public void morSøkerFødselMottarSykepenger() {
+        
+    }
+    
+    
+    public void morSøkerFødselMottarForLite() {
+        
+    }
+    
+    public void morSøkerFødselMottarSykepengerOgInntekter() {
+        
+    }
+    
+    public void morSøkerFødselMilitærtjenesteGodkjent() {
+        
+    }
+    
+    public void morSøkerFødselMilitærtjenesteAvvist() {
+        
+    }
+}
