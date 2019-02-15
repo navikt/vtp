b46a2171f4e8265ec0e903d717837bd300c33a0e
Morten Svendsen <Morten.Svendsen@nav.no>
2019-02-15 12:31:22 +0100

    la til scenarier i tester

--- autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/foreldrepenger/Ytelser.java
+++ autotest/src/test/java/no/nav/foreldrepenger/autotest/foreldrepenger/foreldrepenger/Ytelser.java
@@ -1,29 +1,51 @@
 package no.nav.foreldrepenger.autotest.foreldrepenger.foreldrepenger;
 
+import java.io.IOException;
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import no.nav.foreldrepenger.autotest.aktoerer.Aktoer;
+import no.nav.foreldrepenger.autotest.aktoerer.Aktoer.Rolle;
+import no.nav.foreldrepenger.fpmock2.dokumentgenerator.foreldrepengesoknad.soeknad.ForeldrepengesoknadBuilder;
+import no.nav.foreldrepenger.fpmock2.server.api.scenario.TestscenarioDto;
+import no.nav.foreldrepenger.fpmock2.testmodell.dokument.modell.koder.DokumenttypeId;
 
 @Tag("smoke")
 @Tag("foreldrepenger")
 public class Ytelser extends ForeldrepengerTestBase {
 
-    public void morSøkerFødselMottarSykepenger() {
+    @Test
+    public void morSøkerFødselMottarSykepenger() throws Exception {
+        TestscenarioDto testscenario = opprettScenario("100");
         
+        String søkerAktørIdent = testscenario.getPersonopplysninger().getSøkerAktørIdent();
+        LocalDate fødselsdato = testscenario.getPersonopplysninger().getFødselsdato();
+        LocalDate startDatoForeldrepenger = fødselsdato.minusWeeks(3);
+
+        ForeldrepengesoknadBuilder søknad = foreldrepengeSøknadErketyper.fodselfunnetstedUttakKunMor(søkerAktørIdent, fødselsdato);
+        fordel.erLoggetInnMedRolle(Rolle.SAKSBEHANDLER);
+        long saksnummer = fordel.sendInnSøknad(søknad.build(), testscenario, DokumenttypeId.FOEDSELSSOKNAD_FORELDREPENGER);
     }
     
-    
-    public void morSøkerFødselMottarForLite() {
-        
+    @Test
+    public void morSøkerFødselMottarForLite() throws Exception {
+        TestscenarioDto testscenario = opprettScenario("71");
     }
     
-    public void morSøkerFødselMottarSykepengerOgInntekter() {
-        
+    @Test
+    public void morSøkerFødselMottarSykepengerOgInntekter() throws Exception {
+        TestscenarioDto testscenario = opprettScenario("72");
     }
     
-    public void morSøkerFødselMilitærtjenesteGodkjent() {
-        
+    @Test
+    public void morSøkerFødselMilitærtjenesteGodkjent() throws Exception {
+        TestscenarioDto testscenario = opprettScenario("73");
     }
     
-    public void morSøkerFødselMilitærtjenesteAvvist() {
-        
+    @Test
+    public void morSøkerFødselMilitærtjenesteAvvist() throws Exception {
+        TestscenarioDto testscenario = opprettScenario("70");
     }
 }
