fac54305e1f519f36a371fd18ceba474fb808c00
Martin Sommervold <martin.sommervold@nav.no>
2019-04-01 16:49:49 +0200

    PFP-6422 la til soap-prefiks på soap-tjenester for å frigjøre root contextpath, oppdatert autotest.list. Ny modul front-end med VUE-kode.Ny appkontekst for front-end. Nytt prefiks rest for rest-mocks. POM-kode for å bygge og kopiere vue med yarn over til src for server, slik at html/js blir med i feit jar.

--- server/src/main/java/no/nav/foreldrepenger/fpmock2/server/RestConfig.java
+++ server/src/main/java/no/nav/foreldrepenger/fpmock2/server/RestConfig.java
@@ -28,7 +28,7 @@ public class RestConfig {
 
     public void setup(DelegatingTestscenarioBuilderRepository testScenarioRepository, GsakRepo gsakRepo) {
         // Setup RESTEasy's HttpServletDispatcher at "/api/*".
-        final ServletContextHandler context = new ServletContextHandler(handler, "/");
+        final ServletContextHandler context = new ServletContextHandler(handler, "/rest");
         
         // tilgjengeligjør disse for direkte injeksjon i REST tjenester uten CDI via @Context parameter
         // ref https://stackoverflow.com/questions/21126812/bootstrapping-jax-rs-resteasy
@@ -48,7 +48,7 @@ public class RestConfig {
         ServletHolder servletHolder = new ServletHolder(servlet);
         servletHolder.setInitParameter("resteasy.servlet.mapping.prefix", "/");
         servletHolder.setInitParameter("javax.ws.rs.Application", ApplicationConfig.class.getName());
-        context.addServlet(servletHolder, "/*");
+        context.addServlet(servletHolder, "/rest/*");
     }
 
 }
--- server/src/main/java/no/nav/foreldrepenger/fpmock2/server/SoapWebServiceConfig.java
+++ server/src/main/java/no/nav/foreldrepenger/fpmock2/server/SoapWebServiceConfig.java
@@ -45,40 +45,40 @@ public class SoapWebServiceConfig {
 
     public void setup(TestscenarioBuilderRepository repo, JournalRepository journalRepository, GsakRepo gsakRepo) {
 
-        publishWebService(new SecurityTokenServiceMockImpl(), "/SecurityTokenServiceProvider/");
+        publishWebService(new SecurityTokenServiceMockImpl(), "/soap/SecurityTokenServiceProvider/");
         
         // TODO NB! disse "access wsdl on..." er tvilsomme, da de de returnerer WSDL/XSD *generert* fra JAXB-klassene, ikke originaldokumentene
-        publishWebService(new AktoerServiceMockImpl(repo), "/aktoerregister/ws/Aktoer/v2");
+        publishWebService(new AktoerServiceMockImpl(repo), "/soap/aktoerregister/ws/Aktoer/v2");
         // access wsdl on http://localhost:7999/aktoer?wsdl
-        publishWebService(new SakServiceMockImpl(gsakRepo), "/nav-gsak-ws/SakV1");
+        publishWebService(new SakServiceMockImpl(gsakRepo), "/soap/nav-gsak-ws/SakV1");
         // access wsdl on http://localhost:7999/sak?wsdl
-        publishWebService(new PersonServiceMockImpl(repo), "/tpsws/ws/Person/v3");
+        publishWebService(new PersonServiceMockImpl(repo), "/soap/tpsws/ws/Person/v3");
         // access wsdl on http://localhost:7999/person?wsdl
-        publishWebService(new JournalV2ServiceMockImpl(journalRepository), "/joark/Journal/v2");
+        publishWebService(new JournalV2ServiceMockImpl(journalRepository), "/soap/joark/Journal/v2");
         // access wsdl on http://localhost:7999/journal?wsdl
-        publishWebService(new JournalV3ServiceMockImpl(journalRepository), "/joark/Journal/v3");
-        publishWebService(new InngaaendeJournalServiceMockImpl(journalRepository), "/joark/InngaaendeJournal/v1");
-        // publishWebService(new OppgavebehandlingServiceMockImpl(gsakRepo), "/nav-gsak-ws/BehandleOppgaveV1");
+        publishWebService(new JournalV3ServiceMockImpl(journalRepository), "/soap/joark/Journal/v3");
+        publishWebService(new InngaaendeJournalServiceMockImpl(journalRepository), "/soap/joark/InngaaendeJournal/v1");
+        // publishWebService(new OppgavebehandlingServiceMockImpl(gsakRepo), "/soap/nav-gsak-ws/BehandleOppgaveV1");
         // access wsdl on http://localhost:7999/oppgavebehandling?wsdl
-        publishWebService(new BehandleOppgaveServiceMockImpl(gsakRepo), "/nav-gsak-ws/BehandleOppgaveV1");
+        publishWebService(new BehandleOppgaveServiceMockImpl(gsakRepo), "/soap/nav-gsak-ws/BehandleOppgaveV1");
         // access wsdl on http://localhost:7999/behandleoppgave?wsdl
-        publishWebService(new BehandleSak2ServiceMockImpl(gsakRepo, repo), "/nav-gsak-ws/BehandleSakV2");
+        publishWebService(new BehandleSak2ServiceMockImpl(gsakRepo, repo), "/soap/nav-gsak-ws/BehandleSakV2");
         // access wsdl on http://localhost:7999/behandlesakV2?wsdl
-        publishWebService(new FinnSakListeMockImpl(repo), "/infotrygd-ws/InfotrygdSak/v1");
-        publishWebService(new FinnGrunnlagListeMockImpl(repo), "/infotrygd-ws/InfotrygdBeregningsgrunnlag/v1");
+        publishWebService(new FinnSakListeMockImpl(repo), "/soap/infotrygd-ws/InfotrygdSak/v1");
+        publishWebService(new FinnGrunnlagListeMockImpl(repo), "/soap/infotrygd-ws/InfotrygdBeregningsgrunnlag/v1");
         // access wsdl on http://localhost:7999/infotrygdsak?wsdl
-        publishWebService(new KodeverkServiceMockImpl(repo), "/kodeverk/ws/Kodeverk/v2");
+        publishWebService(new KodeverkServiceMockImpl(repo), "/soap/kodeverk/ws/Kodeverk/v2");
 
-        publishWebService(new DokumentproduksjonV2MockImpl(journalRepository), "/dokprod/ws/dokumentproduksjon/v2");
-        publishWebService(new MeldekortUtbetalingsgrunnlagMockImpl(repo), "/ail_ws/MeldekortUtbetalingsgrunnlag_v1");
-        publishWebService(new MedlemServiceMockImpl(repo), "/medl2/ws/Medlemskap/v2");
-        publishWebService(new ArbeidsfordelingMockImpl(repo), "/norg2/ws/Arbeidsfordeling/v1");
-        publishWebService(new InntektMockImpl(repo), "/inntektskomponenten-ws/inntekt/v3/Inntekt");
-        publishWebService(new OppgaveServiceMockImpl(), "/nav-gsak-ws/OppgaveV3");
-        publishWebService(new ArbeidsforholdMockImpl(repo), "/aareg-core/ArbeidsforholdService/v3");
-        publishWebService(new OrganisasjonMockImpl(repo), "/ereg/ws/OrganisasjonService/v4");
-        publishWebService(new BehandleJournalV3ServiceMockImpl(),"/services/behandlejournal/v3");
-        publishWebService(new SimulerFpServiceMockImpl(), "/cics/services/oppdragService");
+        publishWebService(new DokumentproduksjonV2MockImpl(journalRepository), "/soap/dokprod/ws/dokumentproduksjon/v2");
+        publishWebService(new MeldekortUtbetalingsgrunnlagMockImpl(repo), "/soap/ail_ws/MeldekortUtbetalingsgrunnlag_v1");
+        publishWebService(new MedlemServiceMockImpl(repo), "/soap/medl2/ws/Medlemskap/v2");
+        publishWebService(new ArbeidsfordelingMockImpl(repo), "/soap/norg2/ws/Arbeidsfordeling/v1");
+        publishWebService(new InntektMockImpl(repo), "/soap/inntektskomponenten-ws/inntekt/v3/Inntekt");
+        publishWebService(new OppgaveServiceMockImpl(), "/soap/nav-gsak-ws/OppgaveV3");
+        publishWebService(new ArbeidsforholdMockImpl(repo), "/soap/aareg-core/ArbeidsforholdService/v3");
+        publishWebService(new OrganisasjonMockImpl(repo), "/soap/ereg/ws/OrganisasjonService/v4");
+        publishWebService(new BehandleJournalV3ServiceMockImpl(),"/soap/services/behandlejournal/v3");
+        publishWebService(new SimulerFpServiceMockImpl(), "/soap/cics/services/oppdragService");
     }
 
     private void publishWebService(Object ws, String path, WebServiceFeature... features ) {
