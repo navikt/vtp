ca83ddf768f3954488ad576d62dd01c5fed79cae
Martin Sommervold <martin.sommervold@nav.no>
2019-04-01 16:49:49 +0200

    PFP-6422 la til soap-prefiks på soap-tjenester for å frigjøre root contextpath, oppdatert autotest.list. Ny modul front-end med VUE-kode.Ny appkontekst for front-end. Nytt prefiks rest for rest-mocks. POM-kode for å bygge og kopiere vue med yarn over til src for server, slik at html/js blir med i feit jar.

---
+++ frontend/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    publicPath: './',
+    css: {
+        loaderOptions: {
+            css: {
+                url: false,
+            }
+        }
+    },
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    baseUrl: './',
+    css: {
+        loaderOptions: {
+            css: {
+                url: false,
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
