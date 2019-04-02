21b50c02cd930380d1061c43e970695f29b6a3ff
Martin Sommervold <martin.sommervold@nav.no>
2019-04-02 12:30:25 +0200

    PFP-6422 La til en del vue-relaterte filer som ikke ble med i tidligere commit

---
+++ frontend/src/views/Datascenarier.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Navbar></Navbar>
+    <Start></Start>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Start from '@/components/Start.vue'; // @ is an alias to /src
+import Navbar from '@/components/Navbar.vue';
+
+@Component({
+  components: {
+    Start,
+    Navbar,
+  },
+})
+export default class Datascenarier extends Vue {}
+</script>
