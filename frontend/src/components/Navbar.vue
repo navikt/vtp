126b941728e53d41f3d45e8a6f9b758627217e3f
Martin Sommervold <martin.sommervold@nav.no>
2019-04-02 12:17:02 +0200

    PFP-6422 navbar gui, enkel routing

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">VTP</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item to="/">Datascenarioer</b-nav-item>
+                    <b-nav-item to="/soknader">Søknader</b-nav-item>
+                </b-navbar-nav>
+
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class Navbar extends Vue {
+
+    }
+
+</script>
---
+++ frontend/src/components/Start.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Start extends Vue {
+
+}
+</script>
