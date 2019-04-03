36104a1e4cae6e2c60df4223e896ce6cb5ac309a
Martin Sommervold <martin.sommervold@nav.no>
2019-04-03 16:22:20 +0200

    PFP-6422 ny versjon med javascript

---
+++ frontend/src/views/Datascenarier.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="datascenarier">
+        <Scenariovelger></Scenariovelger>
+        <hr />
+
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Scenariovelger from '@/components/Scenariovelger.vue'
+
+export default {
+  name: 'datascenarier',
+  components: {
+    Scenariovelger
+  }
+}
+</script>
