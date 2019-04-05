09a793d400560abd149c4d5fcaee6159fd6479c2
Martin Sommervold <martin.sommervold@nav.no>
2019-04-05 12:48:06 +0200

    PFP-6422 Vuex store. Init av urler i store for backend i forskjellige kontekster (localhost, deployed, http(s))

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,5 +15,9 @@ Vue.use(BootstrapVue)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created: function() {
+    this.$store.dispatch('setBackendHost') // kontekstavhengig compute av host for backend, i.e. https vs http og localhost vs deployed
+    this.$store.dispatch('setApiPath', '/rest/api') // kontekstavhengig compute av host for backend, i.e. https vs http og localhost vs deployed
+  }
 }).$mount('#app')
