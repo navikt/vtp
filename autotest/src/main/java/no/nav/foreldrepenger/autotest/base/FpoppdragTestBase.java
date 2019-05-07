deb71175636d39ae01a065acc16e46183d71714a
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-05-07 13:17:43 +0200

    FIX fjerner BeforeEach annotation

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/base/FpoppdragTestBase.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/base/FpoppdragTestBase.java
@@ -1,7 +1,5 @@
 package no.nav.foreldrepenger.autotest.base;
 
-import org.junit.jupiter.api.BeforeEach;
-
 import no.nav.foreldrepenger.autotest.aktoerer.fpoppdrag.Saksbehandler;
 import no.nav.foreldrepenger.autotest.klienter.vtp.testscenario.TestscenarioKlient;
 import no.nav.foreldrepenger.autotest.util.http.BasicHttpSession;
@@ -14,8 +12,7 @@ public class FpoppdragTestBase extends TestBase {
 
     protected InntektsmeldingErketype inntektsmeldingErketype;
 
-    @BeforeEach
-    void setUp() throws Exception {
+    public FpoppdragTestBase() throws Exception {
         saksbehandler = new Saksbehandler();
         testscenarioKlient = new TestscenarioKlient(BasicHttpSession.session());
         inntektsmeldingErketype = new InntektsmeldingErketype();
