8ada71f477e7fd0c4410c2519dc6471360098b14
christofferlohne <45653861+christofferlohne@users.noreply.github.com>
2019-05-07 10:50:29 +0200

    Feature/pfp 4355 testcase gradering sn (#28)
    * PFP-4335 Legger til testcase for SN og SNFL med gradering
    
    * PFP-4355 Rydder opp i gradering testscenarior
    
    * PFP-4355 Tar bort unødvendig metode for søknad med opptjening for SNFL

--- dokumentgenerator/src/main/java/no/nav/foreldrepenger/fpmock2/dokumentgenerator/foreldrepengesoknad/erketyper/OpptjeningErketyper.java
+++ dokumentgenerator/src/main/java/no/nav/foreldrepenger/fpmock2/dokumentgenerator/foreldrepengesoknad/erketyper/OpptjeningErketyper.java
@@ -78,6 +78,59 @@ public class OpptjeningErketyper {
         return opptjening;
 
     }
+    public static Opptjening medEgenNaeringOgFrilansOpptjening(){
+
+        Periode periode = new Periode();
+        periode.setFom((LocalDate.now().minusYears(2)));
+        periode.setTom((LocalDate.now()));
+
+        Frilansoppdrag frilansoppdrag = new Frilansoppdrag();
+        frilansoppdrag.setOppdragsgiver("Tims BBQ og fotmassasje");
+        frilansoppdrag.setPeriode(periode);
+
+        Frilans frilans = new Frilans();
+        frilans.setHarInntektFraFosterhjem(false);
+        frilans.setErNyoppstartet(false);
+        frilans.setNaerRelasjon(false);
+        frilans.getPeriode().add(periode);
+        frilans.getFrilansoppdrag().add(frilansoppdrag);
+
+        Opptjening opptjening = new Opptjening();
+        opptjening.setFrilans(frilans);
+
+        List<EgenNaering> naeringer = opptjening.getEgenNaering();
+        NorskOrganisasjon naering = new NorskOrganisasjon();
+
+        Regnskapsfoerer regnskapsfoerer = new Regnskapsfoerer();
+        regnskapsfoerer.setNavn("Regnar Regnskap");
+        regnskapsfoerer.setTelefon("99999999");
+        naering.setRegnskapsfoerer(regnskapsfoerer);
+
+        Periode periodeSN = new Periode();
+        periodeSN.setFom((LocalDate.now().minusYears(4)));
+        periodeSN.setTom((LocalDate.now()));
+        naering.setPeriode(periodeSN);
+
+        List<Virksomhetstyper> typer = naering.getVirksomhetstype();
+        Virksomhetstyper type = new Virksomhetstyper();
+        type.setKode("ANNEN");
+        typer.add(type);
+
+        naering.setOppstartsdato((LocalDate.now().minusYears(4)));
+        naering.setOrganisasjonsnummer("979191138");
+        naering.setNaerRelasjon(false);
+        naering.setNavn("Navnet Organisasjon");
+        naering.setErVarigEndring(true);
+        naering.setBeskrivelseAvEndring("Endringsbeskrivelse");
+        naering.setEndringsDato((LocalDate.now().minusWeeks(1)));
+        naering.setNaeringsinntektBrutto(BigInteger.valueOf(1_500_000));
+        naering.setErNyoppstartet(false);
+        naering.setErNyIArbeidslivet(false);
+
+        naeringer.add(naering);
+        return opptjening;
+
+    }
 
     public static Opptjening medVentelonnVartpengerOpptjening(){
         Opptjening opptjening = new Opptjening();
