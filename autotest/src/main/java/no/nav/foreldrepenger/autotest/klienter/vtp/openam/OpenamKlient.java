18dd7c1ef215883ede96628eb830de187c0ce714
Ola Løvholm <ola.lovholm@nav.no>
2019-05-22 11:14:24 +0200

    NOJIRA: generer cookie lokalt.

--- autotest/src/main/java/no/nav/foreldrepenger/autotest/klienter/vtp/openam/OpenamKlient.java
+++ autotest/src/main/java/no/nav/foreldrepenger/autotest/klienter/vtp/openam/OpenamKlient.java
@@ -1,18 +1,19 @@
 package no.nav.foreldrepenger.autotest.klienter.vtp.openam;
 
-import no.nav.foreldrepenger.autotest.klienter.vtp.VTPKlient;
-import no.nav.foreldrepenger.autotest.klienter.vtp.openam.dto.AccessTokenResponseDTO;
-import no.nav.foreldrepenger.autotest.util.http.HttpSession;
-import no.nav.foreldrepenger.fpmock2.felles.OidcTokenGenerator;
-import no.nav.modig.testcertificates.TestCertificates;
-import org.apache.http.impl.cookie.BasicClientCookie;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.http.impl.cookie.BasicClientCookie;
+
+import no.nav.foreldrepenger.autotest.klienter.vtp.VTPKlient;
+import no.nav.foreldrepenger.autotest.klienter.vtp.openam.dto.AccessTokenResponseDTO;
+import no.nav.foreldrepenger.autotest.util.http.HttpSession;
+import no.nav.foreldrepenger.fpmock2.felles.OidcTokenGenerator;
+import no.nav.modig.testcertificates.TestCertificates;
 
 public class OpenamKlient extends VTPKlient {
 
@@ -52,18 +53,37 @@ public class OpenamKlient extends VTPKlient {
     }
 
     BasicClientCookie createCookie(String rolle) {
-        String token;
-        try {
-            token = fetchToken(rolle);
-        } catch (IOException e) {
-            log.warn("Klarte ikke å hente token fra VTP.");
-            token = generateToken(rolle);
+       /*
+       String token;
+       try {
+           token = fetchToken(rolle);
+       } catch (IOException e) {
+           log.warn("Klarte ikke å hente token fra VTP.");
+           token = generateToken(rolle);
+       }
+       BasicClientCookie cookie = new BasicClientCookie("ID_token", token);
+       cookie.setPath("/");
+       cookie.setDomain("");
+       cookie.setExpiryDate(new Date(LocalDateTime.now().plusHours(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()));
+       return cookie;
+       */
+        String issuer;
+        if(null != System.getenv("ENABLE_CUSTOM_TRUSTSTORE") && System.getenv("ENABLE_CUSTOM_TRUSTSTORE").equalsIgnoreCase("true")) {
+            // @todo Hvor blir dette brukt. Kan det bruke samme instilling som
+            issuer = System.getProperty("isso.oauth2.issuer", "https://fpmock2:8063/rest/isso/oauth2");
+        } else {
+            issuer = System.getProperty("isso.oauth2.issuer", "https://localhost:8063/rest/isso/oauth2"); //fixme med propertyutils
         }
+
+        String token = new OidcTokenGenerator(rolle, "notcetnonce").withIssuer(issuer).create();
+
         BasicClientCookie cookie = new BasicClientCookie("ID_token", token);
         cookie.setPath("/");
         cookie.setDomain("");
         cookie.setExpiryDate(new Date(LocalDateTime.now().plusHours(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()));
         return cookie;
     }
+
+
 
 }
