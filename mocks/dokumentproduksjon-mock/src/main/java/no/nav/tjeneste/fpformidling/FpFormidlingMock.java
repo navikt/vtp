6963279deac5dafb57708d4523d2631575490a4f
olovholm <ola.lovholm@nav.no>
2019-10-01 16:08:51 +0200

    Utvider formidlig mock med flere endepunkter brukt av FPSAK.

--- mocks/dokumentproduksjon-mock/src/main/java/no/nav/tjeneste/fpformidling/FpFormidlingMock.java
+++ mocks/dokumentproduksjon-mock/src/main/java/no/nav/tjeneste/fpformidling/FpFormidlingMock.java
@@ -1,6 +1,6 @@
 package no.nav.tjeneste.fpformidling;
 
-import java.util.Collections;
+import java.util.*;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,12 +10,14 @@ import javax.ws.rs.core.Response;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import no.nav.tjeneste.fpformidling.dto.BehandlingUuidDto;
-import no.nav.tjeneste.fpformidling.dto.HentBrevmalerDto;
+import no.nav.tjeneste.fpformidling.dto.*;
 
 @Api("/fpformidling")
 @Path("/fpformidling")
 public class FpFormidlingMock {
+
+    private final Map<UUID, List<String>> dokumentProduksjon = new HashMap<>();
+    private final Map<UUID, TekstFraSaksbehandlerDto> saksbehandlerTekst = new HashMap<>();
 
     public FpFormidlingMock() {
     }
@@ -26,6 +28,42 @@ public class FpFormidlingMock {
     @ApiOperation(value = "HentDokumentmalListe", notes = ("Returnerer tilgjengelige dokumentmaler"))
     public Response hentDokumentmalListe(BehandlingUuidDto request) {
         return Response.ok(new HentBrevmalerDto(Collections.emptyList())).build();
+    }
+
+    @POST
+    @Path("brev/maler")
+    @Produces("application/json")
+    public HentBrevmalerDto hentBrevmaler(BehandlingUuidDto uuidDto) {
+        return new HentBrevmalerDto(Collections.emptyList());
+    }
+
+    @POST
+    @Path("brev/dokument-sendt")
+    @Produces("application/json")
+    public Boolean erDokumentSendt(DokumentProdusertDto request) {
+        return dokumentProduksjon.getOrDefault(request.getBehandlingUuid(), List.of()).contains(request.getDokumentMal());
+    }
+
+    @POST
+    @Path("brev/bestill")
+    @Produces("application/json")
+    public void bestillDokument(DokumentbestillingDto request) {
+        dokumentProduksjon.putIfAbsent(request.getBehandlingUuid(), new ArrayList<>());
+        dokumentProduksjon.get(request.getBehandlingUuid()).add(request.getDokumentMal());
+    }
+
+    @POST
+    @Path("saksbehandlertekst/hent")
+    @Produces("application/json")
+    public TekstFraSaksbehandlerDto hentSaksbehandlersTekst(BehandlingUuidDto uuidDto) {
+        return saksbehandlerTekst.getOrDefault(uuidDto.getBehandlingUuid(), null);
+    }
+
+    @POST
+    @Path("saksbehandlertekst/lagre")
+    @Produces("application/json")
+    public void lagreSaksbehandlersTekst(TekstFraSaksbehandlerDto request) {
+        saksbehandlerTekst.put(request.getBehandlingUuid(), request);
     }
 
 }
