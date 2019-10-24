dcfdcf9dd194565a21ebd81628d0cf54df42ede4
Christoffer Lohne <christoffer.lohne@soprasteria.com>
2019-10-24 12:49:41 +0200

    Legger til overførinsperiode i FellesErketype

---
+++ dokumentgenerator/src/main/java/no/nav/foreldrepenger/vtp/dokumentgenerator/foreldrepengesoknad/OverføringÅrsak.java
@@ -0,0 +1,9 @@
+package no.nav.foreldrepenger.vtp.dokumentgenerator.foreldrepengesoknad;
+
+public enum OverføringÅrsak {
+    INSTITUSJONSOPPHOLD_ANNEN_FORELDRE,
+    SYKDOM_ANNEN_FORELDER,
+    IKKE_RETT_ANNEN_FORELDER,
+    ALENEOMSORG,
+    UDEFINERT
+}
