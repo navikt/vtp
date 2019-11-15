65bf2469022783f7d4958fd47f7be85c2fd81f09
Geirmund Simonsen <geirmund.simonsen@nav.no>
2019-11-15 11:48:15 +0100

    infotrygdkontantstøttemock rydding

--- mocks/infotrygd-kontantstotte-mock/src/main/java/no/nav/infotrygdks/InfotrygdKontantstotteMock.java
+++ mocks/infotrygd-kontantstotte-mock/src/main/java/no/nav/infotrygdks/InfotrygdKontantstotteMock.java
@@ -4,12 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
