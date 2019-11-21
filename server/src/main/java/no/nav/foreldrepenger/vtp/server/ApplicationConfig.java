c153fffb260c47980c55bca5843cc1d8e015adf5
Tor Garmann Nærland <Tor.Nerland@nav.no>
2019-11-21 22:55:52 +0100

    NOJIRA legger til Medl2 mock Rest-tjeneste

--- server/src/main/java/no/nav/foreldrepenger/vtp/server/ApplicationConfig.java
+++ server/src/main/java/no/nav/foreldrepenger/vtp/server/ApplicationConfig.java
@@ -18,6 +18,7 @@ import no.nav.foreldrepenger.vtp.server.rest.IsReadyImpl;
 import no.nav.foreldrepenger.vtp.server.rest.auth.Oauth2RestService;
 import no.nav.foreldrepenger.vtp.server.rest.auth.PdpRestTjeneste;
 import no.nav.infotrygdfeed.InfotrygdfeedMock;
+import no.nav.medl2.rest.api.v1.MedlemskapsunntakMock;
 import no.nav.sigrun.SigrunMock;
 import no.nav.tjeneste.fpformidling.FpFormidlingMock;
 import no.nav.vtp.DummyRestTjeneste;
@@ -79,7 +80,7 @@ public class ApplicationConfig extends Application {
         classes.add(FpFormidlingMock.class);
         classes.add(DummyRestTjeneste.class);
         classes.add(DummyRestTjenesteBoolean.class);
-
+        classes.add(MedlemskapsunntakMock.class);
         // tekniske ting
         classes.add(Oauth2RestService.class);
         classes.add(PdpRestTjeneste.class);
