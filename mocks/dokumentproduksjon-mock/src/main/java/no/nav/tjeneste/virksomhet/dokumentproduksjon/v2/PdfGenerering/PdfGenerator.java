67e0aefeaa862398a0c38ba392631930a0e5a08a
Espen Waaga <espen.waaga@nav.no>
2020-02-14 14:33:45 +0100

    Legger til font som brukes i formateringen av PDF.

--- mocks/dokumentproduksjon-mock/src/main/java/no/nav/tjeneste/virksomhet/dokumentproduksjon/v2/PdfGenerering/PdfGenerator.java
+++ mocks/dokumentproduksjon-mock/src/main/java/no/nav/tjeneste/virksomhet/dokumentproduksjon/v2/PdfGenerering/PdfGenerator.java
@@ -4,21 +4,27 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class PdfGenerator implements AutoCloseable {
 
+    private final ClassLoader classLoader = PdfGenerator.class.getClassLoader();
+    private static final String FONT = "fonts/OpenSans-Regular.ttf";
+
     final PDDocument doc;
     final String[] pdfcontent;
+    private PDFont font;
     private PDPageContentStream content = null;
     private int textRenderingLineY = 0;
 
     public PdfGenerator(PDDocument doc, String message) {
         this.pdfcontent = message.replaceAll("\t", "  ").split(System.getProperty("line.separator"));
         this.doc = doc;
+        loadAndSetFont();
     }
 
     public void renderText(int marginwidth) throws IOException {
@@ -29,16 +35,24 @@ public class PdfGenerator implements AutoCloseable {
 
     private void renderText(String info, int marginwidth) throws IOException {
 
-            if (content == null || textRenderingLineY < 20)
-                newPage();
+        if (content == null || textRenderingLineY < 20)
+            newPage();
 
-            textRenderingLineY-=10;
-            PDFont fontPlain = PDType1Font.TIMES_ROMAN;
-            content.beginText();
-            content.setFont(fontPlain, 8);
-            content.newLineAtOffset(marginwidth, textRenderingLineY);
-            content.showText(info);
-            content.endText();
+        textRenderingLineY-=10;
+
+        content.beginText();
+        content.setFont(font, 8);
+        content.newLineAtOffset(marginwidth, textRenderingLineY);
+        content.showText(info);
+        content.endText();
+    }
+
+    private void loadAndSetFont() {
+        try (InputStream fontInputStream = classLoader.getResourceAsStream(FONT);){
+            font = PDType0Font.load(doc, fontInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
