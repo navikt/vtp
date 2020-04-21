fc708c51835c97196b60ac2a5b1bb80c11ba9dcf
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2020-04-21 14:40:07 +0200

    Fjerner lagring av pdf og bruker istedenfor outputstream. (#363)

--- mocks/dokumentproduksjon-mock/src/test/java/no/nav/tjeneste/virksomhet/dokumentproduksjon/v2/PdfGeneratorUtilTest.java
+++ mocks/dokumentproduksjon-mock/src/test/java/no/nav/tjeneste/virksomhet/dokumentproduksjon/v2/PdfGeneratorUtilTest.java
@@ -1,24 +1,21 @@
 package no.nav.tjeneste.virksomhet.dokumentproduksjon.v2;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
+import no.nav.tjeneste.virksomhet.dokumentproduksjon.v2.PdfGenerering.PdfGeneratorUtil;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import no.nav.tjeneste.virksomhet.dokumentproduksjon.v2.PdfGenerering.PdfGeneratorUtil;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PdfGeneratorUtilTest {
 
     private static final String INPUT_XML = "brevxml.txt";
     private static final String INPUT_TEXT = "paragraph.txt";
-    private static final String OUTPUT_PDF = "statiskBrev.pdf";
     private static PdfGeneratorUtil renderer;
     private final ClassLoader classLoader = PdfGeneratorUtilTest.class.getClassLoader();
 
@@ -31,29 +28,21 @@ public class PdfGeneratorUtilTest {
     public void PdfGeneratorXmlTest() throws Exception {
         try (var inputStream = classLoader.getResourceAsStream(INPUT_XML)) {
             String inputString = hentStringFraInputStream(inputStream);
-            renderer.genererPdfByteArrayFraString(inputString);
-            try (var fis = new FileInputStream(OUTPUT_PDF); var doc = PDDocument.load(fis)) {
-                int numberOfPages = doc.getNumberOfPages();
-                assertThat(numberOfPages).isEqualTo(2);
-            } catch (IOException e) {
-                throw new IllegalStateException("Kunne ikke laste inn " + OUTPUT_PDF, e);
-            }
+            byte[] byteArray = renderer.genererPdfByteArrayFraString(inputString);
+            var doc = PDDocument.load(byteArray);
+            int numberOfPages = doc.getNumberOfPages();
+            assertThat(numberOfPages).isEqualTo(2);
         }
     }
 
     @Test
-    public void PdfGeneratorLongParagraphTest() {
+    public void PdfGeneratorLongParagraphTest() throws Exception {
         try (var inputStream = classLoader.getResourceAsStream(INPUT_TEXT)) {
             String inputString = hentStringFraInputStream(inputStream);
-            renderer.genererPdfByteArrayFraString(inputString);
-            try (var fis = new FileInputStream(OUTPUT_PDF); var doc = PDDocument.load(fis)) {
-                int numberOfPages = doc.getNumberOfPages();
-                assertThat(numberOfPages).isGreaterThan(1);
-            } catch (IOException e) {
-                throw new IllegalStateException("Kunne ikke laste inn " + OUTPUT_PDF, e);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            byte[] byteArray = renderer.genererPdfByteArrayFraString(inputString);
+            var doc = PDDocument.load(byteArray);
+            int numberOfPages = doc.getNumberOfPages();
+            assertThat(numberOfPages).isGreaterThan(1);
         }
     }
 
