12d51c96877bf47897d7a9bd6a545a7d7bbe15cc
Espen Waaga <espen.waaga@nav.no>
2020-07-02 16:27:47 +0200

    FIX: Endre variabelnavn for å matche med JsonProperty

--- model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/inntektytelse/trex/Arbeidsforhold.java
+++ model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/inntektytelse/trex/Arbeidsforhold.java
@@ -10,18 +10,18 @@ public class Arbeidsforhold {
 
     private final Orgnummer orgnr;
     private final Integer inntekt;
-    private final Inntektsperiode inntektperiode;
+    private final Inntektsperiode inntektsperiode;
     private final Boolean refusjon;
 
     @JsonCreator
     public Arbeidsforhold(
             @JsonProperty("orgnr") @JsonAlias("arbeidsgiverOrgnr") Orgnummer orgnr,
             @JsonProperty("inntekt") @JsonAlias("inntektForPerioden") Integer inntekt,
-            @JsonProperty("inntektperiode") Inntektsperiode inntektperiode,
+            @JsonProperty("inntektsperiode") Inntektsperiode inntektsperiode,
             @JsonProperty("refusjon") Boolean refusjon) {
         this.orgnr = orgnr;
         this.inntekt = inntekt;
-        this.inntektperiode = inntektperiode;
+        this.inntektsperiode = inntektsperiode;
         this.refusjon = refusjon;
     }
 
@@ -33,8 +33,8 @@ public class Arbeidsforhold {
         return inntekt;
     }
 
-    public Inntektsperiode getInntektperiode() {
-        return inntektperiode;
+    public Inntektsperiode getInntektsperiode() {
+        return inntektsperiode;
     }
 
     public Boolean getRefusjon() {
@@ -43,7 +43,7 @@ public class Arbeidsforhold {
 
     @Override
     public int hashCode() {
-        return Objects.hash(orgnr, inntekt, inntektperiode, refusjon);
+        return Objects.hash(orgnr, inntekt, inntektsperiode, refusjon);
     }
 
     @Override
@@ -57,13 +57,13 @@ public class Arbeidsforhold {
         Arbeidsforhold that = (Arbeidsforhold) obj;
         return Objects.equals(that.orgnr, this.orgnr) &&
                 Objects.equals(that.inntekt, this.inntekt) &&
-                Objects.equals(that.inntektperiode, this.inntektperiode) &&
+                Objects.equals(that.inntektsperiode, this.inntektsperiode) &&
                 Objects.equals(that.refusjon, this.refusjon);
     }
 
     @Override
     public String toString() {
         return getClass().getSimpleName() + "[orgnr=" + orgnr + ", inntekt=" + inntekt + ", inntektperiode="
-                + inntektperiode + ", refusjon=" + refusjon + "]";
+                + inntektsperiode + ", refusjon=" + refusjon + "]";
     }
 }
