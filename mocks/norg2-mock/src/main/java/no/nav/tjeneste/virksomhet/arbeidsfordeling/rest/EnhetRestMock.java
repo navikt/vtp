5b1775a9fbb0379d32250b398dddca39107ba737
Anton Fofanov <anton.fofanov@nav.no>
2020-08-06 10:45:49 +0200

    legge til norg2 endepunkt for å hente enkelte enheter

--- mocks/norg2-mock/src/main/java/no/nav/tjeneste/virksomhet/arbeidsfordeling/rest/EnhetRestMock.java
+++ mocks/norg2-mock/src/main/java/no/nav/tjeneste/virksomhet/arbeidsfordeling/rest/EnhetRestMock.java
@@ -1,61 +1,75 @@
 package no.nav.tjeneste.virksomhet.arbeidsfordeling.rest;
 
-import static java.util.Collections.singletonList;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import no.nav.norg2.model.Norg2RsEnhet;
 
-import java.util.List;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.util.List;
 
-import io.swagger.annotations.ApiParam;
+import static java.util.Collections.singletonList;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
-import no.nav.norg2.model.Norg2RsEnhet;
-
+@Api(tags = {"Norg2 enheter"})
 @Path("/norg2/api/v1/enhet")
 public class EnhetRestMock {
     @GET
-    @Produces({ "application/json;charset=UTF-8" })
-    @io.swagger.annotations.ApiOperation(value = "Returnerer en filtrert liste av alle enheter", response = Norg2RsEnhet.class, responseContainer = "List", tags={ "enhet", })
+    @Produces({"application/json;charset=UTF-8"})
+    @io.swagger.annotations.ApiOperation(value = "Returnerer en filtrert liste av alle enheter", response = Norg2RsEnhet.class, responseContainer = "List", tags = {"enhet",})
     @io.swagger.annotations.ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 200, message = "Success", response = Norg2RsEnhet.class, responseContainer = "List"),
             @io.swagger.annotations.ApiResponse(code = 400, message = "Bad request"),
             @io.swagger.annotations.ApiResponse(code = 404, message = "Not found"),
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error") })
-    public Response getAllEnheterUsingGET(@ApiParam(value = "Enhetsstatus resulterende enheter skal filtreres på", allowableValues="UNDER_ETABLERING, AKTIV, UNDER_AVVIKLING, NEDLAGT") @QueryParam("enhetStatusListe") List<String> enhetStatusListe
-            ,@ApiParam(value = "Enhetsnumre for filtrering av enheter") @QueryParam("enhetsnummerListe") List<String> enhetsnummerListe
-            ,@ApiParam(value = "Hvorvidt enheter skal være oppgavebehandlende", allowableValues="UFILTRERT, KUN_OPPGAVEBEHANDLERE, INGEN_OPPGAVEBEHANDLERE") @QueryParam("oppgavebehandlerFilter") String oppgavebehandlerFilter
-            ,@Context SecurityContext securityContext)
+            @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")})
+    public Response getAllEnheterUsingGET(@ApiParam(value = "Enhetsstatus resulterende enheter skal filtreres på", allowableValues = "UNDER_ETABLERING, AKTIV, UNDER_AVVIKLING, NEDLAGT") @QueryParam("enhetStatusListe") List<String> enhetStatusListe
+            , @ApiParam(value = "Enhetsnumre for filtrering av enheter") @QueryParam("enhetsnummerListe") List<String> enhetsnummerListe
+            , @ApiParam(value = "Hvorvidt enheter skal være oppgavebehandlende", allowableValues = "UFILTRERT, KUN_OPPGAVEBEHANDLERE, INGEN_OPPGAVEBEHANDLERE") @QueryParam("oppgavebehandlerFilter") String oppgavebehandlerFilter
+            , @Context SecurityContext securityContext)
             throws NotFoundException {
 
-        return Response.ok(singletonList(enEnhet())).build();
+        return Response.ok(singletonList(enEnhet(4407L))).build();
     }
 
     @GET
     @Path("/navkontor/{geografiskOmraade}")
-    @Produces({ "application/json;charset=UTF-8" })
-    @io.swagger.annotations.ApiOperation(value = "Returnerer en NAV kontor som er en lokalkontor for spesifisert geografisk område", response = Norg2RsEnhet.class, tags={ "enhet", })
+    @Produces({"application/json;charset=UTF-8"})
+    @io.swagger.annotations.ApiOperation(value = "Returnerer en NAV kontor som er en lokalkontor for spesifisert geografisk område", response = Norg2RsEnhet.class, tags = {"enhet",})
     @io.swagger.annotations.ApiResponses(value = {
             @io.swagger.annotations.ApiResponse(code = 200, message = "Success", response = Norg2RsEnhet.class),
             @io.swagger.annotations.ApiResponse(code = 404, message = "Not Found"),
-            @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error") })
-    public Response getEnhetByGeografiskOmraadeUsingGET(@ApiParam(value = "Geografisk identifikator for NAV kontoret",required=true) @PathParam("geografiskOmraade") String geografiskOmraade
-            ,@ApiParam(value = "Diskresjonskode på saker et NAV kontor kan behandle", allowableValues="SPFO, SPSF, ANY") @QueryParam("disk") String disk
-            ,@Context SecurityContext securityContext)
+            @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")})
+    public Response getEnhetByGeografiskOmraadeUsingGET(@ApiParam(value = "Geografisk identifikator for NAV kontoret", required = true) @PathParam("geografiskOmraade") String geografiskOmraade
+            , @ApiParam(value = "Diskresjonskode på saker et NAV kontor kan behandle", allowableValues = "SPFO, SPSF, ANY") @QueryParam("disk") String disk
+            , @Context SecurityContext securityContext)
             throws NotFoundException {
 
-        return Response.ok(enEnhet()).build();
+        return Response.ok(enEnhet(4407L)).build();
     }
 
-    private Norg2RsEnhet enEnhet() {
+    @GET
+    @Path("/{enhetsnummer}")
+    @Produces({"application/json;charset=UTF-8"})
+    @io.swagger.annotations.ApiOperation(value = "Returnerer en enhet basert på enhetsnummer", response = Norg2RsEnhet.class, tags = {"enhet",})
+    @io.swagger.annotations.ApiResponses(value = {
+            @io.swagger.annotations.ApiResponse(code = 200, message = "Success", response = Norg2RsEnhet.class),
+            @io.swagger.annotations.ApiResponse(code = 404, message = "Not Found"),
+            @io.swagger.annotations.ApiResponse(code = 500, message = "Internal Server Error")})
+    public Response getEnhetUsingGET(@ApiParam(value = "Enhetsnummeret til enheten oppslaget gjelder for", required = true) @PathParam("enhetsnummer") String enhetsnummer,
+                                     @Context SecurityContext securityContext) throws NotFoundException {
+        try {
+            return Response.ok(enEnhet(Long.parseLong(enhetsnummer))).build();
+        } catch (final NumberFormatException e) {
+            return Response.status(BAD_REQUEST).build();
+        }
+    }
+
+
+    private Norg2RsEnhet enEnhet(long enhetId) {
         Norg2RsEnhet norg2RsEnhet = new Norg2RsEnhet();
-        norg2RsEnhet.setEnhetId(4407L);
+        norg2RsEnhet.setEnhetId(enhetId);
         norg2RsEnhet.setNavn("NAV Arbeid og ytelser Tønsberg");
         norg2RsEnhet.setOppgavebehandler(true);
         return norg2RsEnhet;
