36a53ff94f571b1b86f21667c22187d703c8c000
Espen Waaga <espen.waaga@nav.no>
2020-10-16 13:19:34 +0200

    Fikser og refakturerer FamilieRelasjonBygger. Dødfødsel blir ikke lenger regisrert som en familierelasjon, men DoedfoedtBarn.

--- mocks/pdl-mock/src/main/java/no/nav/pdl/oversetter/FamilieRelasjonBygger.java
+++ mocks/pdl-mock/src/main/java/no/nav/pdl/oversetter/FamilieRelasjonBygger.java
@@ -10,6 +10,7 @@ import no.nav.foreldrepenger.vtp.testmodell.personopplysning.FamilierelasjonMode
 import no.nav.foreldrepenger.vtp.testmodell.personopplysning.Personopplysninger;
 import no.nav.pdl.DoedfoedtBarn;
 import no.nav.pdl.Familierelasjon;
+import no.nav.pdl.Familierelasjonsrolle;
 import no.nav.pdl.Person;
 
 public class FamilieRelasjonBygger {
@@ -20,41 +21,59 @@ public class FamilieRelasjonBygger {
     public static Person byggFamilierelasjoner(String aktørIdent, Personopplysninger personopplysningerModell, Person person) {
         person.setFamilierelasjoner(new ArrayList<>()); // init array
 
-        boolean erBarnet = false;
-        List<DoedfoedtBarn> doedfoedtBarnList = new ArrayList<>();
-        for (FamilierelasjonModell relasjon : personopplysningerModell.getFamilierelasjoner()) {
-            if(relasjon.getRolle().equals(FamilierelasjonModell.Rolle.BARN) && relasjon.getTil().getAktørIdent().equals(aktørIdent)) {
-                erBarnet = true;
-            } else if (relasjon.getRolle().equals(FamilierelasjonModell.Rolle.BARN)) {
-                DoedfoedtBarn doedfoedtBarn = new DoedfoedtBarn();
-                BarnModell barnModell = (BarnModell) relasjon.getTil();
-                LocalDate fødselsdato = barnModell.getFødselsdato();
-                if (barnModell.getFødselsdato() != null && erFdatNummer(barnModell.getIdent())) {
-                    doedfoedtBarn.setDato(fødselsdato.format(DATO_FORMATTERER));
-                }
-                doedfoedtBarnList.add(doedfoedtBarn);
-            }
-        }
-        person.setDoedfoedtBarn(doedfoedtBarnList);
+        boolean erBarnet = erIdentenBarnet(aktørIdent, personopplysningerModell);
+        leggTilDoedfoedtBarnForRelasjonerMedFDATIdent(personopplysningerModell, person, erBarnet);
 
         List<Familierelasjon> familierelasjoner;
-        if(personopplysningerModell.getAnnenPart() != null && personopplysningerModell.getAnnenPart().getAktørIdent().equals(aktørIdent)) { //TODO HACK for annenpart (annenpart burde ha en egen personopplysning fil eller liknende)
-            familierelasjoner = FamilierelasjonAdapter.tilFamilerelasjon(personopplysningerModell.getFamilierelasjonerForAnnenPart());
-            familierelasjoner.forEach(fr -> person.getFamilierelasjoner().add(fr));
-        }
-        else if(erBarnet) { //TODO HACK Familierelasjon for barnet
+        if(erBarnet) { //TODO HACK Familierelasjon for barnet
             familierelasjoner = FamilierelasjonAdapter.tilFamilerelasjon(personopplysningerModell.getFamilierelasjonerForBarnet());
             familierelasjoner.forEach(fr -> person.getFamilierelasjoner().add(fr));
+        } else if(personopplysningerModell.getAnnenPart() != null && personopplysningerModell.getAnnenPart().getAktørIdent().equals(aktørIdent)) { //TODO HACK for annenpart (annenpart burde ha en egen personopplysning fil eller liknende)
+            familierelasjoner = FamilierelasjonAdapter.tilFamilerelasjon(personopplysningerModell.getFamilierelasjonerForAnnenPart());
+            leggTilFamilierelasjonHvisRelasjonenIkkeErDødfødtBarn(person, familierelasjoner);
         }
         else {
             familierelasjoner = FamilierelasjonAdapter.tilFamilerelasjon(personopplysningerModell.getFamilierelasjoner());
-            familierelasjoner.forEach(fr -> person.getFamilierelasjoner().add(fr));
+            leggTilFamilierelasjonHvisRelasjonenIkkeErDødfødtBarn(person, familierelasjoner);
         }
 
         for (Familierelasjon familierelasjon : familierelasjoner) {
             System.out.println("    " + familierelasjon.getRelatertPersonsIdent());
         }
         return person;
+    }
+
+    private static void leggTilFamilierelasjonHvisRelasjonenIkkeErDødfødtBarn(Person person, List<Familierelasjon> familierelasjoner) {
+        familierelasjoner.stream()
+                .filter(familierelasjon -> !(familierelasjon.getRelatertPersonsRolle().equals(Familierelasjonsrolle.BARN) && erFdatNummer(familierelasjon.getRelatertPersonsIdent())))
+                .forEach(familierelasjon -> person.getFamilierelasjoner().add(familierelasjon));
+    }
+
+    private static boolean erIdentenBarnet(String aktørIdent, Personopplysninger personopplysningerModell) {
+        for (FamilierelasjonModell relasjon : personopplysningerModell.getFamilierelasjoner()) {
+            if (relasjon.getRolle().equals(FamilierelasjonModell.Rolle.BARN) && relasjon.getTil().getAktørIdent().equals(aktørIdent)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void leggTilDoedfoedtBarnForRelasjonerMedFDATIdent(Personopplysninger personopplysningerModell, Person person, boolean erBarnet) {
+        List<DoedfoedtBarn> doedfoedtBarnList = new ArrayList<>();
+        if (!erBarnet) {
+            for (FamilierelasjonModell relasjon : personopplysningerModell.getFamilierelasjoner()) {
+                if (relasjon.getRolle().equals(FamilierelasjonModell.Rolle.BARN)) {
+                    DoedfoedtBarn doedfoedtBarn = new DoedfoedtBarn();
+                    BarnModell barnModell = (BarnModell) relasjon.getTil();
+                    LocalDate fødselsdato = barnModell.getFødselsdato();
+                    if (barnModell.getFødselsdato() != null && erFdatNummer(barnModell.getIdent())) {
+                        doedfoedtBarn.setDato(fødselsdato.format(DATO_FORMATTERER));
+                    }
+                    doedfoedtBarnList.add(doedfoedtBarn);
+                }
+            }
+        }
+        person.setDoedfoedtBarn(doedfoedtBarnList);
     }
 
     private static String getPersonnummer(String str) {
@@ -70,5 +89,4 @@ public class FamilieRelasjonBygger {
     private static boolean erFdatNummer(String ident) {
         return isFdatNummer(getPersonnummer(ident));
     }
-
 }
