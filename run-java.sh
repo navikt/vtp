8cde8df38d3408b403f967359f7a34436411ac00
Marius Bækken Glittum <marius.glittum@nav.no>
2020-10-20 09:29:30 +0200

    Update run-java.sh

--- run-java.sh
+++ run-java.sh
@@ -4,12 +4,14 @@ if [[ $MSYSTEM =~ MINGW* ]]; then
 	set -ex
 	(cd server && exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp 'target/app.jar;target/lib/*' \
 		-Dlogback.configurationFile=logback.xml \
+		-XX:MaxRAMPercentage=75.0 \
 		-Dfile.encoding=UTF8 \
 		-Duser.timezone=Europe/Oslo \
 		no.nav.foreldrepenger.vtp.server.MockServer)
 else
 	exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* \
 		-Dlogback.configurationFile=logback.xml \
+		-XX:MaxRAMPercentage=75.0 \
 		-Dfile.encoding=UTF8 \
 		-Duser.timezone=Europe/Oslo \
 		no.nav.foreldrepenger.vtp.server.MockServer
