4239e0074d3a6e07ca4504e7bb89de3d23b961d0
Joakim Schweizer <joakim.schweizer@nav.no>
2020-10-28 15:45:06 +0100

    Signed commites test

--- mocks/oppdrag-mock/src/main/java/no/nav/system/os/eksponering/SimuleringGenerator.java
+++ mocks/oppdrag-mock/src/main/java/no/nav/system/os/eksponering/SimuleringGenerator.java
@@ -212,7 +212,6 @@ public class SimuleringGenerator {
         //1.Ytelsen slik den stod original
         //2.Feilutbetalt beløp
         //3.Fjerning av ytelsen fra seqence 1
-        //Signing test
 
         stoppnivaaDetaljer.setFaktiskFom(dateTimeFormatter.format(periode.getFom()));
         stoppnivaaDetaljer.setFaktiskTom(dateTimeFormatter.format(periode.getTom()));
