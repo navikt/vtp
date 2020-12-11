93238b84ac93b6f3ce6717813892869a76db47c2
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-12-11 15:55:51 +0100

    fix: la JAVAX_NET_SSL_KEYSTOREPASSWORD ta presedens over modig passord

--- core/src/main/java/no/nav/foreldrepenger/vtp/felles/KeystoreUtils.java
+++ core/src/main/java/no/nav/foreldrepenger/vtp/felles/KeystoreUtils.java
@@ -15,6 +15,9 @@ public class KeystoreUtils {
     }
 
     public static String getKeystoreFilePath() {
+        if (null != getenv("JAVAX_NET_SSL_KEYSTORE")) {
+            return getenv("JAVAX_NET_SSL_KEYSTORE");
+        }
         if (null != getenv("NO_NAV_MODIG_SECURITY_APPCERT_KEYSTORE")) {
             return getenv("NO_NAV_MODIG_SECURITY_APPCERT_KEYSTORE");
         }
@@ -22,6 +25,9 @@ public class KeystoreUtils {
     }
 
     public static String getKeyStorePassword() {
+        if (null != getenv("JAVAX_NET_SSL_KEYSTOREPASSWORD")) {
+            return getenv("JAVAX_NET_SSL_KEYSTOREPASSWORD");
+        }
         if (null != getenv("NO_NAV_MODIG_SECURITY_APPCERT_PASSWORD")) {
             return getenv("NO_NAV_MODIG_SECURITY_APPCERT_PASSWORD");
         }
