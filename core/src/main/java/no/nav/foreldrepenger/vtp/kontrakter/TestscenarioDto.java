9092741524c15cc9f8756ee923cf2f941809fbea
Espen Waaga <espen.waaga@nav.no>
2020-12-29 13:50:59 +0100

    Fjerner unlødvendige jackson annoteringer for testscenarioDto

--- core/src/main/java/no/nav/foreldrepenger/vtp/kontrakter/TestscenarioDto.java
+++ core/src/main/java/no/nav/foreldrepenger/vtp/kontrakter/TestscenarioDto.java
@@ -2,21 +2,16 @@ package no.nav.foreldrepenger.vtp.kontrakter;
 
 import java.util.Map;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import no.nav.foreldrepenger.vtp.testmodell.inntektytelse.InntektYtelseModell;
 
-@JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
 public record TestscenarioDto(
-        @JsonProperty("templateKey") String templateKey,
-        @JsonProperty("templateNavn") String templateNavn,
+        String templateKey,
+        String templateNavn,
         @JsonProperty("id") String testscenarioId,
-        @JsonProperty("variabler") @JsonInclude(content = Include.NON_EMPTY)  Map<String, String> variabler,
-        @JsonProperty("personopplysninger") TestscenarioPersonopplysningDto personopplysninger,
+        Map<String, String> variabler,
+        TestscenarioPersonopplysningDto personopplysninger,
         @JsonProperty("scenariodata") InntektYtelseModell scenariodataDto,
-        @JsonProperty("scenariodataAnnenpart") @JsonInclude(content = Include.NON_EMPTY) InntektYtelseModell scenariodataAnnenpartDto) {
+        @JsonProperty("scenariodataAnnenpart") InntektYtelseModell scenariodataAnnenpartDto) {
 }
--- core/src/main/java/no/nav/foreldrepenger/vtp/kontrakter/TestscenarioPersonopplysningDto.java
+++ core/src/main/java/no/nav/foreldrepenger/vtp/kontrakter/TestscenarioPersonopplysningDto.java
@@ -3,16 +3,11 @@ package no.nav.foreldrepenger.vtp.kontrakter;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 public record TestscenarioPersonopplysningDto(
-        @JsonProperty("søkerIdent") String søkerIdent,
-        @JsonProperty("søkerAktørIdent") String søkerAktørIdent,
-        @JsonProperty("annenpartIdent") String annenpartIdent,
-        @JsonProperty("annenpartAktørIdent") String annenpartAktørIdent,
-        @JsonProperty("fødselsdato") LocalDate fødselsdato,
-        @JsonProperty("barnIdenter") @JsonIgnoreProperties(ignoreUnknown = true) List<String> barnIdenter) {
+        String søkerIdent,
+        String søkerAktørIdent,
+        String annenpartIdent,
+        String annenpartAktørIdent,
+        LocalDate fødselsdato,
+        List<String> barnIdenter) {
 }
