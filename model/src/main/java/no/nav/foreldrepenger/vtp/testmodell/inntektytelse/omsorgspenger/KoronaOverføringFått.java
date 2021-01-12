b6df14182b77d4449e8184e6d454b321f4fca415
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2021-01-12 14:28:07 +0100

    Fra/til ist. for avsender/mottaker i response (#653)

--- model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/inntektytelse/omsorgspenger/KoronaOverføringFått.java
+++ model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/inntektytelse/omsorgspenger/KoronaOverføringFått.java
@@ -9,15 +9,15 @@ public record KoronaOverføringFått(LocalDate gjennomført,
                              LocalDate gyldigFraOgMed,
                              LocalDate gyldigTilOgMed,
                              Duration lengde,
-                             Person avsender,
+                             Person fra,
                              List<Kilde> kilder) {
 
-    public KoronaOverføringFått(LocalDate gjennomført, LocalDate gyldigFraOgMed, LocalDate gyldigTilOgMed, Duration lengde, Person avsender, List<Kilde> kilder) {
+    public KoronaOverføringFått(LocalDate gjennomført, LocalDate gyldigFraOgMed, LocalDate gyldigTilOgMed, Duration lengde, Person fra, List<Kilde> kilder) {
         this.gjennomført = gjennomført;
         this.gyldigFraOgMed = gyldigFraOgMed;
         this.gyldigTilOgMed = gyldigTilOgMed;
         this.lengde = lengde;
-        this.avsender = avsender;
+        this.fra = fra;
         this.kilder = Optional.ofNullable(kilder).orElse(List.of());
     }
 }
