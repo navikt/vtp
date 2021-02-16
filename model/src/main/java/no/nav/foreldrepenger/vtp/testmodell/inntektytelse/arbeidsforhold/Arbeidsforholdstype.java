0e2acba21cb07dbc6f721928b8b3d7ca229c9552
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2021-02-16 16:29:26 +0100

    Arbeidsforholdstype for frilans

--- model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/inntektytelse/arbeidsforhold/Arbeidsforholdstype.java
+++ model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/inntektytelse/arbeidsforhold/Arbeidsforholdstype.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum Arbeidsforholdstype {
     ORDINÆRT_ARBEIDSFORHOLD("ordinaertArbeidsforhold"),
     MARITIMT_ARBEIDSFORHOLD("maritimtArbeidsforhold"),
+    FRILANSER_OPPDRAGSTAKER_MED_MER("frilanserOppdragstakerHonorarPersonerMm"),
     FORENKLET_OPPGJØRSORDNING("forenkletOppgjoersordning");
 
     private final String kode;
