a620b4da21ac43af9fd96bc6a9836a5f297d7ba9
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2021-02-26 15:36:12 +0100

    Testscenario returner kjønn. (#704)

--- core/src/main/java/no/nav/foreldrepenger/vtp/kontrakter/TestscenarioPersonopplysningDto.java
+++ core/src/main/java/no/nav/foreldrepenger/vtp/kontrakter/TestscenarioPersonopplysningDto.java
@@ -8,13 +8,16 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(ignoreUnknown = true) 
+import no.nav.foreldrepenger.vtp.testmodell.personopplysning.BrukerModell;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
-public record TestscenarioPersonopplysningDto(
-                                              @JsonProperty("søkerIdent") String søkerIdent,
+public record TestscenarioPersonopplysningDto(@JsonProperty("søkerIdent") String søkerIdent,
                                               @JsonProperty("søkerAktørIdent") String søkerAktørIdent,
+                                              @JsonProperty("søkerKjønn") BrukerModell.Kjønn søkerKjønn,
                                               @JsonProperty("annenpartIdent") String annenpartIdent,
                                               @JsonProperty("annenpartAktørIdent") String annenpartAktørIdent,
+                                              @JsonProperty("annenpartKjønn") BrukerModell.Kjønn annenpartKjønn,
                                               @JsonProperty("fødselsdato") LocalDate fødselsdato,
                                               @JsonProperty("barnIdenter") List<String> barnIdenter,
                                               @JsonProperty("barnIdentAktørId") Map<String, String> barnIdentAktørId) {
