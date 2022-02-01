219fc784c9c07fb5a1618e4fe444300fb0ed0697
Espen Waaga <espen.waaga@nav.no>
2022-02-01 14:06:38 +0100

    FIX: Pesysmock feil variabelnavn på dto..

--- mocks/pesys-mock/src/main/java/no/nav/mock/pesys/dto/UforehistorikkDto.java
+++ mocks/pesys-mock/src/main/java/no/nav/mock/pesys/dto/UforehistorikkDto.java
@@ -2,6 +2,6 @@ package no.nav.mock.pesys.dto;
 
 import java.util.List;
 
-public record UforehistorikkDto(List<UforeperiodeDto> uføreperiodeListe) {
+public record UforehistorikkDto(List<UforeperiodeDto> uforeperiodeListe) {
 
 }
