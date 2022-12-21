0c4594d030657a588c59a8e532dfd6b6dde558ed
Espen Waaga <espen.waaga@nav.no>
2022-12-21 09:35:46 +0100

    TFP-5333: Legger til mock av iversetting av tilbakekrevingsvedtak + annulering av kravgrunnlag

--- mocks/fpwsproxy-mock/src/main/java/no/nav/foreldrepenger/fpwsproxy/tilbakekreving/FpWsProxyTilbakekrevingMock.java
+++ mocks/fpwsproxy-mock/src/main/java/no/nav/foreldrepenger/fpwsproxy/tilbakekreving/FpWsProxyTilbakekrevingMock.java
@@ -4,7 +4,9 @@ package no.nav.foreldrepenger.fpwsproxy.tilbakekreving;
 import static no.nav.foreldrepenger.fpwsproxy.tilbakekreving.Kravgrunnlag431DtoMapper.lagGeneriskKravgrunnlag;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -15,6 +17,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.swagger.annotations.ApiOperation;
+import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.iverksett.TilbakekrevingVedtakDTO;
+import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.kravgrunnlag.request.AnnullerKravGrunnlagDto;
 import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.kravgrunnlag.request.HentKravgrunnlagDetaljDto;
 import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.kravgrunnlag.respons.Kravgrunnlag431Dto;
 
@@ -22,9 +26,11 @@ import no.nav.foreldrepenger.kontrakter.fpwsproxy.tilbakekreving.kravgrunnlag.re
 public class FpWsProxyTilbakekrevingMock {
     private static final Logger LOG = LoggerFactory.getLogger(FpWsProxyTilbakekrevingMock.class);
 
+    private static final String KRAVGRUNNLAG_PATH = "/kravgrunnlag";
+    private static final String TILBAKEKREVINGVEDTAK_PATH = "/tilbakekrevingsvedtak";
 
     @POST
-    @Path("/kravgrunnlag")
+    @Path(KRAVGRUNNLAG_PATH)
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "", notes = "Hent kravgrunnlag", response = Kravgrunnlag431Dto[].class)
@@ -34,7 +40,24 @@ public class FpWsProxyTilbakekrevingMock {
                 .build();
     }
 
+    @DELETE
+    @Path(KRAVGRUNNLAG_PATH)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "", notes = "Annuler kravgrunnlag", response = Kravgrunnlag431Dto[].class)
+    public Response annulerKravgrunnlag(@Valid AnnullerKravGrunnlagDto annullerKravGrunnlagDto) {
+        LOG.info("Annuler grunnlag med vedtaksId {}", annullerKravGrunnlagDto.vedtakId());
+        return Response.ok().build();
+    }
 
-
-
+    @POST
+    @Path(TILBAKEKREVINGVEDTAK_PATH)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "", notes = "Iverksett tilbakekrevingsvedtak", response = TilbakekrevingVedtakDTO[].class)
+    public Response hentKravgrunnlag(@Valid @NotNull TilbakekrevingVedtakDTO tilbakekrevingVedtakDTO) {
+        LOG.info("Iverksetter tilbakekrevingsvedtak for {}", tilbakekrevingVedtakDTO.vedtakId());
+        return Response.ok(tilbakekrevingVedtakDTO)
+                .build();
+    }
 }
