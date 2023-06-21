a42d3f038694a0a3b4749317cdc3f5ebc1bac7cb
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-06-21 15:53:07 +0200

    Fjerner en copy&past feil MicrosoftGraphApiMock.java

--- server/src/main/java/no/nav/foreldrepenger/vtp/server/auth/rest/azureAD/MicrosoftGraphApiMock.java
+++ server/src/main/java/no/nav/foreldrepenger/vtp/server/auth/rest/azureAD/MicrosoftGraphApiMock.java
@@ -9,7 +9,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-import io.swagger.v3.oas.annotations.tags.Tag;docker
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "AzureAd")
 @Path("/MicrosoftGraphApi")
