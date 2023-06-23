9462b0482c30a8a10a52fbb0f887321249e18e3e
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-06-23 13:34:48 +0200

    Sommerrengjøring: Rydder opp i pom filer og avhengigheter 1 (#1168)
    * Sommerrengjøring: Rydder opp i pom filer og avhengigheter 1
    
    * jersey-common bort
    
    * jersey-common RuntimeDelegateImpl test

--- mocks/pdl-mock/src/main/java/no/nav/pdl/PdlFunctionalException.java
+++ mocks/pdl-mock/src/main/java/no/nav/pdl/PdlFunctionalException.java
@@ -3,22 +3,14 @@ package no.nav.pdl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.http.HttpStatus;
-
 import graphql.ErrorType;
 import graphql.GraphQLError;
 import graphql.language.SourceLocation;
 
 public class PdlFunctionalException extends RuntimeException implements GraphQLError {
-	private final HttpStatus httpStatus;
-
-	public HttpStatus getHttpStatus() {
-		return httpStatus;
-	}
 
 	public PdlFunctionalException(String message, Throwable cause) {
 		super(message, cause);
-		this.httpStatus = null;
 	}
 
 	@Override
