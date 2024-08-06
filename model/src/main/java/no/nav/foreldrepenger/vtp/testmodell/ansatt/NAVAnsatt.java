f272ac984f56f947d7a17f6fc4158605c41760d2
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-08-06 19:52:15 +0200

    Legg til fornavn, etternavn og enhet for ansatte (#1466)
    * Legg til fornavn, etternavn og enhet for ansatte
    
    * Et par security-warnings

--- model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/ansatt/NAVAnsatt.java
+++ model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/ansatt/NAVAnsatt.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.vtp.testmodell.ansatt;
 import java.util.List;
 import java.util.UUID;
 
-public record NAVAnsatt(String ident, UUID oid, String displayName, String email, List<NAVGroup> groups, List<String> enheter) {
+public record NAVAnsatt(String ident, UUID oid, String displayName, String givenName, String surname, String email,
+                        String streetAddress, List<NAVGroup> groups, List<String> enheter) {
     public record NAVGroup(UUID oid, String name) {}
 }
