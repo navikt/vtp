a1819284f3b561c98ed6f877453b2162fea83841
espenwaaga <espen.waaga@nav.no>
2024-11-07 12:13:25 +0100

    Azure: wonderwall krever nå at id_token_signing_alg_values_supported er satt med støttede algoritmer.

--- server/src/main/java/no/nav/foreldrepenger/vtp/server/auth/rest/WellKnownResponse.java
+++ server/src/main/java/no/nav/foreldrepenger/vtp/server/auth/rest/WellKnownResponse.java
@@ -1,4 +1,14 @@
 package no.nav.foreldrepenger.vtp.server.auth.rest;
 
-public record WellKnownResponse(String issuer, String authorization_endpoint, String jwks_uri, String token_endpoint) {
+import java.util.List;
+
+public record WellKnownResponse(String issuer,
+                                String authorization_endpoint,
+                                String jwks_uri,
+                                String token_endpoint,
+                                List<String> id_token_signing_alg_values_supported) {
+
+    public WellKnownResponse(String issuer, String authorization_endpoint, String jwks_uri, String token_endpoint) {
+        this(issuer, authorization_endpoint, jwks_uri, token_endpoint, List.of("RS256"));
+    }
 }
