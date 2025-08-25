754f78bc56a6630ddccea8b79317088d0bc8fce4
Espen Velsvik <espen.velsvik@nav.no>
2025-08-25 20:37:59 +0200

    Feiler dersom aktørid er null

--- mocks/medl-mock/src/main/java/no/nav/medl2/rest/api/v1/MedlemskapsunntakAdapter.java
+++ mocks/medl-mock/src/main/java/no/nav/medl2/rest/api/v1/MedlemskapsunntakAdapter.java
@@ -3,6 +3,7 @@ package no.nav.medl2.rest.api.v1;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import no.nav.foreldrepenger.vtp.testmodell.medlemskap.MedlemskapperiodeModell;
@@ -19,31 +20,24 @@ public class MedlemskapsunntakAdapter {
     }
 
     public List<Medlemskapsunntak> finnMedlemsunntak(String aktørId) {
-        if (aktørId != null) {
-            var brukerModell = scenarioRepository.getPersonIndeks().finnByAktørIdent(aktørId);
-            if (brukerModell instanceof PersonModell pm) {
-                List<Medlemskapsunntak> periodeListe = new ArrayList<>();
-                if(pm.getMedlemskap() != null && pm.getMedlemskap().getPerioder() != null) {
-                    pm.getMedlemskap().getPerioder().forEach(medlemsskapsperiode -> periodeListe.add(tilMedlemsperiode(medlemsskapsperiode)));
-                }
-                return periodeListe;
+        Objects.requireNonNull(aktørId);
+        var brukerModell = scenarioRepository.getPersonIndeks().finnByAktørIdent(aktørId);
+        if (brukerModell instanceof PersonModell pm) {
+            List<Medlemskapsunntak> periodeListe = new ArrayList<>();
+            if (pm.getMedlemskap() != null && pm.getMedlemskap().getPerioder() != null) {
+                pm.getMedlemskap()
+                        .getPerioder()
+                        .forEach(medlemsskapsperiode -> periodeListe.add(tilMedlemsperiode(medlemsskapsperiode)));
             }
+            return periodeListe;
         }
         return Collections.emptyList();
     }
 
     private Medlemskapsunntak tilMedlemsperiode(MedlemskapperiodeModell medlPeriode) {
-        return new Medlemskapsunntak(
-                medlPeriode.id(),
-                medlPeriode.fom(),
-                medlPeriode.tom(),
-                medlPeriode.trygdedekning().kode(),
-                null,
-                medlPeriode.lovvalgType().name(),
-                medlPeriode.land().getKode(),
-                null,
+        return new Medlemskapsunntak(medlPeriode.id(), medlPeriode.fom(), medlPeriode.tom(), medlPeriode.trygdedekning().kode(), null,
+                medlPeriode.lovvalgType().name(), medlPeriode.land().getKode(), null,
                 Set.of(PeriodeStatus.INNV, PeriodeStatus.GYLD).contains(medlPeriode.status()),
-                new Medlemskapsunntak.Sporingsinformasjon(medlPeriode.besluttetDato(), medlPeriode.kilde().name()),
-                null);
+                new Medlemskapsunntak.Sporingsinformasjon(medlPeriode.besluttetDato(), medlPeriode.kilde().name()), null);
     }
 }
