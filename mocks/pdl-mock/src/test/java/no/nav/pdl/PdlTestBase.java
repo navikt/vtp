ff66fff0f177ddc084ba48f5a589a2ee9400cb03
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-09-07 10:09:12 +0200

    chore: Jakarta EE 11 (#1678)

--- mocks/pdl-mock/src/test/java/no/nav/pdl/PdlTestBase.java
+++ mocks/pdl-mock/src/test/java/no/nav/pdl/PdlTestBase.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -22,7 +21,6 @@ public abstract class PdlTestBase {
         return new ObjectMapper()
                 .registerModule(new Jdk8Module())
                 .registerModule(new JavaTimeModule())
-                .setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE)
                 .setTimeZone(TimeZone.getTimeZone("Europe/Oslo"))
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .disable(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
