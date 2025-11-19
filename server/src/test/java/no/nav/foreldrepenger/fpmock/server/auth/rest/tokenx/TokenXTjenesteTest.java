28be3ae8ea8efcb6fe2e29858dabb700c47ac1a0
johannbm <johannes.moskvil@gmail.com>
2025-11-19 13:25:17 +0100

    revert

--- server/src/test/java/no/nav/foreldrepenger/fpmock/server/auth/rest/tokenx/TokenXTjenesteTest.java
+++ server/src/test/java/no/nav/foreldrepenger/fpmock/server/auth/rest/tokenx/TokenXTjenesteTest.java
@@ -47,37 +47,37 @@ class TokenXTjenesteTest {
 
     }
 
-//    @Test
-//    @Disabled // Mangler keystore i ´pipe... Legge denne til eller fjerne test? TODO
-//    public void verifisererTokenSomGenereresHarRiktigAudienceOgSubject() throws JoseException, MalformedClaimException {
-//        when(req.getScheme()).thenReturn("http");
-//        when(req.getServerPort()).thenReturn(8060);
-//        var subject_token = """
-//                eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vYWFkYjJjL3YyLjAiLCJleHAiOjE2MzU1MTIxNDMsImp0aSI6IllZNmh1T19reWJPTDFsWmxnZFp3T3ciLCJpYXQiOjE2MzU0OTA1NDMsInN1YiI6IjE3NDk4ODMyODU3IiwiYXVkIjoiT0lEQyIsImFjciI6IkxldmVsNCIsImF6cCI6Ik9JREMifQ.vM6sjTD5uRX25yqtNrUiue3DwWaXA-xkVW52ied3n2MHqfXB-pd76r9LyqDJ-sDZen-AhxihL6GkNLUrXlRm8Ij-FZaJsIQaOnj2CRRHlJfL4Zj-s0i2B81ov0qJclJKtZFhFELwWkqykqu1IB7W2s7_5d2k0C1jAQCs9gnzCoKv6H8EPgrRriWRvMv691DuqzluPLLsqMK7cwQmODkc3fN4gtoW4Wf6FqG2XDdR6Mr-OT-w_exn8i0uB8jPieSOfyby5CG7rXLW_BQT9KovIrDuwYCyEkU7hSCEYksu5CBSoBpupKs-07cUXIksLWouCZcLjGoMWfnG-rHeTq0w-g
-//                """;
-//        var audience = "fpfordel";
-//        var response = tokenxRestTjeneste.token(
-//                req,
-//                null,
-//                null,
-//                null,
-//                null,
-//                subject_token,
-//                audience);
-//
-//        var tokenExchangeResponse = (TokenExchangeResponse) response.getEntity();
-//        var jwtClaims = jwt(tokenExchangeResponse.access_token());
-//        assertThat(jwtClaims.getSubject()).isEqualTo("17498832857");
-//        assertThat(jwtClaims.getAudience())
-//                .hasSize(1)
-//                .contains(audience);
-//    }
-//
-//    private JwtClaims jwt(String access_token) {
-//        try {
-//            return TokenxRestTjeneste.UNVALIDATING_CONSUMER.processToClaims(access_token);
-//        } catch (InvalidJwtException e) {
-//            throw new RuntimeException("Ikke gyldig access_token!");
-//        }
-//    }
+    @Test
+    @Disabled // Mangler keystore i ´pipe... Legge denne til eller fjerne test? TODO
+    public void verifisererTokenSomGenereresHarRiktigAudienceOgSubject() throws JoseException, MalformedClaimException {
+        when(req.getScheme()).thenReturn("http");
+        when(req.getServerPort()).thenReturn(8060);
+        var subject_token = """
+                eyJraWQiOiIxIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vYWFkYjJjL3YyLjAiLCJleHAiOjE2MzU1MTIxNDMsImp0aSI6IllZNmh1T19reWJPTDFsWmxnZFp3T3ciLCJpYXQiOjE2MzU0OTA1NDMsInN1YiI6IjE3NDk4ODMyODU3IiwiYXVkIjoiT0lEQyIsImFjciI6IkxldmVsNCIsImF6cCI6Ik9JREMifQ.vM6sjTD5uRX25yqtNrUiue3DwWaXA-xkVW52ied3n2MHqfXB-pd76r9LyqDJ-sDZen-AhxihL6GkNLUrXlRm8Ij-FZaJsIQaOnj2CRRHlJfL4Zj-s0i2B81ov0qJclJKtZFhFELwWkqykqu1IB7W2s7_5d2k0C1jAQCs9gnzCoKv6H8EPgrRriWRvMv691DuqzluPLLsqMK7cwQmODkc3fN4gtoW4Wf6FqG2XDdR6Mr-OT-w_exn8i0uB8jPieSOfyby5CG7rXLW_BQT9KovIrDuwYCyEkU7hSCEYksu5CBSoBpupKs-07cUXIksLWouCZcLjGoMWfnG-rHeTq0w-g
+                """;
+        var audience = "fpfordel";
+        var response = tokenxRestTjeneste.token(
+                req,
+                null,
+                null,
+                null,
+                null,
+                subject_token,
+                audience);
+
+        var tokenExchangeResponse = (TokenExchangeResponse) response.getEntity();
+        var jwtClaims = jwt(tokenExchangeResponse.access_token());
+        assertThat(jwtClaims.getSubject()).isEqualTo("17498832857");
+        assertThat(jwtClaims.getAudience())
+                .hasSize(1)
+                .contains(audience);
+    }
+
+    private JwtClaims jwt(String access_token) {
+        try {
+            return TokenxRestTjeneste.UNVALIDATING_CONSUMER.processToClaims(access_token);
+        } catch (InvalidJwtException e) {
+            throw new RuntimeException("Ikke gyldig access_token!");
+        }
+    }
 }
