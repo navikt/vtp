f2278bd2325fd895d35586b9ea41ebd1a4446559
Anders Østby <anders.ostby@nav.no>
2026-03-13 10:49:58 +0100

    Gjør om koder i modell fra string til enum (#1776)
    * Gjør om koder i modell fra string til enum
    
    * Legger til manglende verdier fra no.nav.saf.Kanal
    
    * Bruker enum-type på kanal i OpprettJournalpostRequest med default verdi
    
    * Setter kanal til UKJENT hvis den er null

--- model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/dokument/modell/koder/DokumentTilknyttetJournalpost.java
+++ model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/dokument/modell/koder/DokumentTilknyttetJournalpost.java
@@ -1,43 +1,6 @@
 package no.nav.foreldrepenger.vtp.testmodell.dokument.modell.koder;
 
-import java.util.List;
-import java.util.Objects;
-
-import com.fasterxml.jackson.annotation.JsonValue;
-
-public class DokumentTilknyttetJournalpost {
-
-    private static final List<String> VALID_KODER = List.of("HOVEDDOKUMENT", "VEDLEGG");
-
-    public static final DokumentTilknyttetJournalpost HOVEDDOKUMENT = new DokumentTilknyttetJournalpost("HOVEDDOKUMENT");
-    public static final DokumentTilknyttetJournalpost VEDLEGG = new DokumentTilknyttetJournalpost("VEDLEGG");
-
-    @JsonValue
-    private String kode;
-
-    DokumentTilknyttetJournalpost(String kode){
-        this.kode = kode;
-        if(kode != null && !VALID_KODER.contains(kode)){
-            throw new IllegalArgumentException("Kode er ikke implementert i Joark dokumentTilknyttetJournalpost: " + kode);
-        }
-    }
-
-    public String getKode() {return kode;}
-
-    public void setKode(String kode){this.kode = kode;}
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        return obj instanceof DokumentTilknyttetJournalpost dok && Objects.equals(getKode(), dok.getKode());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getKode());
-    }
-
+public enum DokumentTilknyttetJournalpost {
+    HOVEDDOKUMENT,
+    VEDLEGG
 }
