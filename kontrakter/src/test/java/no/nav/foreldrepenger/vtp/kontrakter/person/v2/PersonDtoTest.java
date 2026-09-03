7654803790d66fa38ae913eba0cb31c93a3efbed
johannbm <johannes.moskvil@gmail.com>
2026-09-03 13:35:43 +0200

    Fjern avhengighet

--- kontrakter/src/test/java/no/nav/foreldrepenger/vtp/kontrakter/person/v2/PersonDtoTest.java
+++ kontrakter/src/test/java/no/nav/foreldrepenger/vtp/kontrakter/person/v2/PersonDtoTest.java
@@ -2,48 +2,9 @@ package no.nav.foreldrepenger.vtp.kontrakter.person.v2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
-import tools.jackson.databind.json.JsonMapper;
-
 class PersonDtoTest {
-
-    private static final JsonMapper MAPPER = JsonMapper.builder().build();
-
-    @Test
-    void skalRundtrippeRegistrerteNæringsvirksomheter() {
-        var virksomhet = new RegistrertNæringsvirksomhetDto(
-                "999999999",
-                "VTP FISKE",
-                "ENK",
-                "Enkeltpersonforetak",
-                "03.110",
-                "Hav- og kystfiske");
-        var person = PersonDto.builder()
-                .registrerteNæringsvirksomheter(List.of(virksomhet))
-                .build();
-
-        var json = MAPPER.writeValueAsString(person);
-        var rundtrippet = MAPPER.readValue(json, PersonDto.class);
-
-        assertThat(rundtrippet.registrerteNæringsvirksomheter()).containsExactly(virksomhet);
-    }
-
-    @Test
-    void skalGiTomListeNårFeltetMangler() {
-        var person = MAPPER.readValue("{}", PersonDto.class);
-
-        assertThat(person.registrerteNæringsvirksomheter()).isEmpty();
-    }
-
-    @Test
-    void skalGiTomListeNårFeltetErNull() {
-        var person = MAPPER.readValue("{\"registrerteNæringsvirksomheter\":null}", PersonDto.class);
-
-        assertThat(person.registrerteNæringsvirksomheter()).isEmpty();
-    }
 
     @Test
     void beholderFemargumentskonstruktør() {
