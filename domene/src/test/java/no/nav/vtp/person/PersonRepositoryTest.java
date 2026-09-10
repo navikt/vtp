f59503dae69fec18310e5b1bbb2f721dfb6de2d5
Johannes Moskvil <johannbm@users.noreply.github.com>
2026-09-10 14:13:37 +0200

    Gjør org mock registrert til brreg også tilgjengelig i ereg mock (#1891)

--- domene/src/test/java/no/nav/vtp/person/PersonRepositoryTest.java
+++ domene/src/test/java/no/nav/vtp/person/PersonRepositoryTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import no.nav.vtp.PersonBuilder;
+import no.nav.vtp.person.ident.Orgnummer;
 import no.nav.vtp.person.næring.RegistrertNæringsvirksomhet;
 
 class PersonRepositoryTest {
@@ -23,5 +24,23 @@ class PersonRepositoryTest {
         var resultat = PersonRepository.hentRegistrertNæringsvirksomhet(virksomhet.organisasjonsnummer());
 
         assertThat(resultat).contains(virksomhet);
+    }
+
+    @Test
+    void registrertNæringsvirksomhetErTilgjengeligForOrganisasjonsoppslag() {
+        var virksomhet = new RegistrertNæringsvirksomhet(
+                "999999999", "VTP FISKE", "ENK", "Enkeltpersonforetak", "03.110", "Hav- og kystfiske");
+        var person = PersonBuilder.lagSøker().tilBuilder()
+                .medRegistrerteNæringsvirksomheter(List.of(virksomhet))
+                .build();
+        PersonRepository.leggTilPerson(person);
+
+        var organisasjon = PersonRepository.hentInformasjonOmArbeidsforhold(
+                new Orgnummer(virksomhet.organisasjonsnummer()));
+
+        assertThat(organisasjon).get().satisfies(org -> {
+            assertThat(org.orgnummer().value()).isEqualTo("999999999");
+            assertThat(org.informasjon().navn()).isEqualTo("VTP FISKE");
+        });
     }
 }
